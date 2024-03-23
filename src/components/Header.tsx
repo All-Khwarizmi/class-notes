95b1677636231433b83232d43ca8a6918336aa94
Jason Suarez <swarecito@gmail.com>
2024-03-23 23:16:53 +0100

    Add link to home page in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,19 @@ import { UserButton, SignInButton, useSession } from "@clerk/nextjs";
 import { ModeToggle } from "./ModeToggle";
 import { Button } from "./ui/button";
 import Navigation from "./Navigation";
+import Link from "next/link";
 
 export default function Header() {
   const { isSignedIn } = useSession();
   return (
     <header className="flex flex-row gap-4 border-b items-center justify-between border-b-slate-300 p-4">
       <Navigation />
-      <h1 className="text-2xl font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient">
+      <Link
+        href={"/"}
+        className="text-2xl font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient"
+      >
         ClassAI
-      </h1>
+      </Link>
       <div className="flex flex-row gap-4 items-center">
         <div>
           {isSignedIn ? (
