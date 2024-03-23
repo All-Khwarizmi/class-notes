dceea3668383a65c313a1c40de4c4a9ec0036d89
Jason Suarez <swarecito@gmail.com>
2024-03-23 17:03:00 +0100

    Refactor page component to use ClassesTable component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,12 @@
 "use client";
 import Header from "@/components/Header";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
+
+import ClassesTable from "@/components/ClassesTable";
 export default function Home() {
-  const classes = useQuery(api.classes.getClasses);
   return (
     <>
       <Header />
-      <div>
-        <h1>Classes</h1>
-        <ul>
-          {classes?.map((c) => (
-            <li key={c.id}>
-              <h2>{c.name}</h2>
-              <p>{c.description}</p>
-              <img src={c.imageUrl} alt={c.name} />
-              {/* <ul>
-                {c.students.map((s) => (
-                  <li key={s}>{s}</li>
-                ))}
-              </ul> */}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ClassesTable />
     </>
   );
 }
---
+++ src/components/ClassesTable.tsx
@@ -0,0 +1,45 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { Button } from "./ui/button";
+export default function ClassesTable() {
+  const classes = useQuery(api.classes.getClasses);
+
+  return (
+    <section className="mt-12 px-4">
+      <Table>
+        <TableCaption>Voici vos classes</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Image</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {classes?.map((c) => (
+            <TableRow key={c._id}>
+              <TableCell>{c.name}</TableCell>
+              <TableCell>{c.description}</TableCell>
+              <TableCell>
+                <img src={c.imageUrl} alt={c.name} />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className="flex justify-center pt-4">
+        {" "}
+        <Button>+</Button>
+      </div>
+    </section>
+  );
+}
