a710349a7d05c4d2125c95a91ab8d342a567e364
Jason Suarez <swarecito@gmail.com>
2024-03-23 19:53:47 +0100

    Add conditional rendering for classes table based on user sign-in status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 import Header from "@/components/Header";
 import Image from "next/image";
+import { useSession } from "@clerk/nextjs";
 
 import ClassesTable from "@/components/ClassesTable";
 export default function Home() {
+  const { isSignedIn } = useSession();
   return (
     <>
       <Header />
-      <ClassesTable />
+      {isSignedIn ? (
+        <ClassesTable />
+      ) : (
+        <section
+          aria-label="Sign in message display"
+          className="flex flex-col items-center justify-center h-[90vh]"
+        >
+          <h1 className="font-bold text-2xl">Sign in to see your classes</h1>
+        </section>
+      )}
     </>
   );
 }
