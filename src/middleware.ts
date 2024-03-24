3f9f0ce7c564424cec0e03630d6a64b1dc21dd89
Jason Suarez <swarecito@gmail.com>
2024-03-24 18:38:29 +0100

    Add middleware file to src path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
+
+/* 
+https://nextjs.org/docs/app/building-your-application/authentication
+*/
 
 export default authMiddleware({
   publicRoutes: ["/", "/contact"],
+  
 });
-
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
