3f807252e90661f4152e1b35fc5a98be22e62db3
Jason Suarez <swarecito@gmail.com>
2024-03-25 22:09:03 +0100

    Add viewport meta tag to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" suppressHydrationWarning>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
+        />
+      </head>
       <Providers>
         <body className={inter.className}>
           <ThemeProvider
