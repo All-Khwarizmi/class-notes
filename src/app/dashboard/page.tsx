00cb8e4eaf986214b76962dda4866de353b79e62
Jason Suarez <swarecito@gmail.com>
2024-04-01 19:49:26 +0200

    Add useAuthStore hook and user repository import

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,10 +3,16 @@ import CustomDialog from "@/components/common/CustomDialog";
 import useAuth from "@/core/auth/useAuth";
 import OnboardingForm from "./OboardingForm";
 import { useEffect, useState } from "react";
+import { useAuthStore } from "@/core/auth/auth-store";
+import { userRepositry } from "../user/repository/user-repository";
 
 export default function Page() {
   const [open, setOpen] = useState(false);
-  const { onboarding, setOnboarding, preferences, setPreferences } = useAuth();
+  useAuth();
+  const { onboarding } = useAuthStore((state) => ({
+    onboarding : state.onboarding,
+  }));
+
   useEffect(() => {
     if (!onboarding) {
       setTimeout(() => setOpen(true), 3000);
