d31ea98eea1f038c4066cf83b3e228e05127ffee
Jason Suarez <swarecito@gmail.com>
2024-04-03 22:29:35 +0200

    Delete unused files and update evaluation criteria schema

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,9 +16,6 @@ import type {
 } from "convex/server";
 import type * as classes from "../classes.js";
 import type * as criteria from "../criteria.js";
-import type * as criteria_dynamic_fields_link from "../criteria_dynamic_fields_link.js";
-import type * as dynamic_fields from "../dynamic_fields.js";
-import type * as evaluation_criteria from "../evaluation_criteria.js";
 import type * as evaluation_template from "../evaluation_template.js";
 import type * as evaluation_with_grades from "../evaluation_with_grades.js";
 import type * as students from "../students.js";
@@ -35,9 +32,6 @@ import type * as users from "../users.js";
 declare const fullApi: ApiFromModules<{
   classes: typeof classes;
   criteria: typeof criteria;
-  criteria_dynamic_fields_link: typeof criteria_dynamic_fields_link;
-  dynamic_fields: typeof dynamic_fields;
-  evaluation_criteria: typeof evaluation_criteria;
   evaluation_template: typeof evaluation_template;
   evaluation_with_grades: typeof evaluation_with_grades;
   students: typeof students;
