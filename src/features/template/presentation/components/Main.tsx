f0cbbb4e12cb30ea7cf2bef7fdd054a46ca96b75
Jason Suarez <swarecito@gmail.com>
2024-04-08 00:47:32 +0200

    implement AddTemplateForm component

--- src/features/template/presentation/components/Main.tsx
+++ src/features/template/presentation/components/Main.tsx
@@ -3,24 +3,29 @@ import { Button } from "@/core/components/ui/button";
 import EvaluationGrid from "./TemplateGrid";
 import { templatesRepository } from "../../application/repository/templates-repository";
 import authRepositoy from "@/features/auth/application/repository/auth-repository";
+import CustomDialog from "@/core/components/common/CustomDialog";
+import AddIcon from "@/core/components/icons/AddIcon";
+import AddTemplateForm from "./AddTemplateForm";
+import { useTemplateCreationStore } from "../../common/template-store";
+import WarningIcon from "@/core/components/icons/WarningIcon";
 
 export default function Main() {
   const { authUserId } = authRepositoy.useGetUserId();
-  const { setIsCreating } = templatesRepository.useStartTemplateCreation();
-
+  const { isCreating } = useTemplateCreationStore((state) => ({
+    isCreating: state.isCreating,
+  }));
   //! Add loading skeleton
   return (
     <section className="h-full w-full flex flex-col gap-4 md:justify-between justify-around p-4">
       <EvaluationGrid userId={authUserId || ""} />
       <footer className="flex justify-center pb-4">
-        <Button
-          onClick={() => {
-            setIsCreating();
-          }}
-          variant="default"
+        <CustomDialog
+          icon={isCreating ? <WarningIcon /> : <AddIcon />}
+          title="Ajouter une évaluation"
+          testId="add-template-dialog"
         >
-          Créer Évaluation
-        </Button>
+          <AddTemplateForm />
+        </CustomDialog>
       </footer>
     </section>
   );
