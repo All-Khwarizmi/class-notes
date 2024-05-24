13130c5da2008d7ac9c1153ec364fafc6f4891b5
Jason Suarez <swarecito@gmail.com>
2024-05-24 20:14:59 +0200

    feat: Add useSaveUser hook for saving user data

---
+++ src/features/profile/application/adapters/services/useSaveUser.ts
@@ -0,0 +1,57 @@
+import useFetchingState from "@/core/application/common/useFetchingState";
+import { useEffect, useState } from "react";
+import { profileUseCases } from "../../usecases/profile-usecases";
+
+import { useRouter } from "next/navigation";
+import { isLeft } from "fp-ts/lib/Either";
+import { toast } from "sonner";
+
+export interface SaveUserOptions {
+  schoolSubject: string;
+  name: string;
+  userId: string;
+}
+
+export default function useSaveUser() {
+  const [saveUserOptions, setSaveUserOptions] =
+    useState<SaveUserOptions | null>();
+  const { loading, error, startFetching, endFetching, setFetchingError } =
+    useFetchingState();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!saveUserOptions) {
+      return;
+    }
+    const loaddindToast = toast.loading("Saving user...", {
+      position: "top-center",
+    });
+
+    startFetching();
+
+    profileUseCases
+      .saveUser({ userId: saveUserOptions.userId, user: saveUserOptions })
+      .then((result) => {
+        if (isLeft(result)) {
+          setFetchingError(result.left.message);
+          return;
+        }
+
+        toast.dismiss(loaddindToast);
+        toast.success("User saved successfully", { position: "top-center" });
+
+        endFetching();
+        router.refresh();
+      })
+
+      .finally(() => {
+        toast.dismiss(loaddindToast);
+      });
+  }, [saveUserOptions]);
+
+  return {
+    setSaveUserOptions,
+    loading,
+    error,
+  };
+}
