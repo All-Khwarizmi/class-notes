1706245cab8c5869e3876f452974888d90713f71
Jason Suarez <swarecito@gmail.com>
2024-05-24 19:42:05 +0200

    feat: Add saveUser method to IDatabase interface

--- src/core/data/idatabase.ts
+++ src/core/data/idatabase.ts
@@ -1,6 +1,7 @@
 import { Either } from "fp-ts/lib/Either";
 import { DocumentData } from "./database-types";
 import Failure from "../failures/failures";
+import { UserType } from "@/features/user/domain/entities/user-schema";
 
 export default abstract class IDatabase {
   abstract getUser({
@@ -8,4 +9,12 @@ export default abstract class IDatabase {
   }: {
     userId: string;
   }): Promise<Either<Failure<string>, DocumentData>>;
+
+  abstract saveUser({
+    userId,
+    user,
+  }: {
+    userId: string;
+    user: UserType;
+  }): Promise<Either<Failure<string>, void>>;
 }
