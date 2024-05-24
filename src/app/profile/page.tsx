cdf2dbae6edcfc4327ff2042c42a12aaefadf5e2
Jason Suarez <swarecito@gmail.com>
2024-05-24 20:34:40 +0200

    feat: Add lazy loading for images in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,10 @@
+import LoadingPage from "@/core/components/common/LoadingPage";
 import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
 import { profileUseCases } from "@/features/profile/application/usecases/profile-usecases";
 import UserProfile from "@/features/profile/presentation/views/UserProfile";
 import { isLeft } from "fp-ts/lib/Either";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 export default async function Page() {
   const authUser = await authUseCases.getUserAuth();
   if (isLeft(authUser)) {
@@ -13,17 +15,19 @@ export default async function Page() {
   });
 
   return (
-    <UserProfile
-      user={
-        isLeft(user)
-          ? {
-              name: "",
-              schoolSubject: "",
-              _id: authUser.right.userId,
-              onboarding: false,
-            }
-          : user.right
-      }
-    />
+    <Suspense fallback={<LoadingPage />}>
+      <UserProfile
+        user={
+          isLeft(user)
+            ? {
+                name: "",
+                schoolSubject: "",
+                _id: authUser.right.userId,
+                onboarding: false,
+              }
+            : user.right
+        }
+      />
+    </Suspense>
   );
 }
