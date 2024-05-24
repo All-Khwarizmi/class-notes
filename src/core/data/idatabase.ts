38bb12ef07d72858511ea28b348d4d08dce96f18
Jason Suarez <swarecito@gmail.com>
2024-05-24 22:23:05 +0200

    feat: Add getCategories and addCategory methods to IDatabase
    The code changes in `idatabase.ts` add two new abstract methods to the `IDatabase` class: `getCategories` and `addCategory`. These methods are responsible for retrieving and adding categories for a specific user. This addition is necessary to support the category functionality in the application.

--- src/core/data/idatabase.ts
+++ src/core/data/idatabase.ts
@@ -2,6 +2,7 @@ import { Either } from "fp-ts/lib/Either";
 import { DocumentData } from "./database-types";
 import Failure from "../failures/failures";
 import { UserType } from "@/features/user/domain/entities/user-schema";
+import { Category } from "@/features/comp-cat/domain/entities/schemas";
 
 export default abstract class IDatabase {
   abstract getUser({
@@ -16,5 +17,19 @@ export default abstract class IDatabase {
   }: {
     userId: string;
     user: UserType;
+  }): Promise<Either<Failure<string>, void>>;
+
+  abstract getCategories({
+    userId,
+  }: {
+    userId: string;
+  }): Promise<Either<Failure<string>, DocumentData[]>>;
+
+  abstract addCategory({
+    userId,
+    category,
+  }: {
+    userId: string;
+    category: Category;
   }): Promise<Either<Failure<string>, void>>;
 }
