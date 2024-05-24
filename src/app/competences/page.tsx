30aa9cff42e651347a33ab79b813e49505db61a1
Jason Suarez <swarecito@gmail.com>
2024-05-24 23:51:21 +0200

    feat: Update layout to include padding for children
    The code changes in `layout.tsx` update the layout component to include a padding of 8 pixels on the top and bottom, and 6 pixels on the left and right for the `children` element. This modification improves the visual spacing and enhances the overall user experience of the competences page.

--- src/app/competences/page.tsx
+++ src/app/competences/page.tsx
@@ -1,8 +1,29 @@
-export default function Page() {
-  
+import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
+import { compCatUsecases } from "@/features/comp-cat/application/usecases/comp-cat-usecases";
+import CompetencesTable from "@/features/comp-cat/presentation/views/CompetencesTable";
+import { isLeft } from "fp-ts/lib/Either";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const authUser = await authUseCases.getUserAuth();
+  if (isLeft(authUser)) {
+    redirect("/login");
+  }
+  const compAndCat = await compCatUsecases.getCategoriesAndCompetences({
+    userId: authUser.right.userId,
+  });
+  if (isLeft(compAndCat)) {
+    console.error(compAndCat.left);
+    return <div>Something went wrong</div>;
+  }
+
   return (
-    <div>
-      <h1>Competences</h1>
-    </div>
+    <>
+      <CompetencesTable
+        competences={compAndCat.right.competences}
+        categories={compAndCat.right.categories}
+        userId={authUser.right.userId}
+      />
+    </>
   );
 }
