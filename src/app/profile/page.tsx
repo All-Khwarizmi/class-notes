76b547892495e4e78448aaa1197fa2689165ae1c
Jason Suarez <swarecito@gmail.com>
2024-05-24 18:11:48 +0200

    feat: Add getUserQuery to fetch user data by ID

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,17 @@
-export default function Page() {
+import { fetchQuery } from "convex/nextjs";
+import { api } from "../../../convex/_generated/api";
+export default async function Page() {
+  const user = await fetchQuery(api.users.getUserQuery, {
+    userId: "user_2eBWORVbe9p5HUGoR9JjAtBRGpU",
+  });
+
+  if (!user) {
+    return <h1>User not found</h1>;
+  }
   return (
     <div>
-      <h1>User </h1>
+      <h1>{user.name}</h1>
+      <h2>{user.schoolSubject}</h2>
     </div>
   );
 }
