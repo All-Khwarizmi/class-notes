d990976e892872da4e27a4221335944484e3e380
Jason Suarez <swarecito@gmail.com>
2024-05-25 19:29:05 +0200

    refactor: Update CoursView component to display course name and description
    This commit updates the CoursView component in the cours/presentation/views directory. It adds the course name and description to the component's JSX, using the provided `cours` object. This change improves the user experience by displaying relevant information about the course on the page.

--- src/features/cours/presentation/views/CoursView.tsx
+++ src/features/cours/presentation/views/CoursView.tsx
@@ -1,3 +1,19 @@
-export default async function CoursView({}: {}) {
-  return <div>Cours Page: </div>;
+import { Edit } from "lucide-react";
+import { Cours } from "../../domain/entities/cours-schemas";
+import EditorProviderWrapper from "../components/EditorProvider";
+
+export default async function CoursView({
+  cours,
+}: {
+  cours: Cours;
+  userId: string;
+}) {
+  return (
+    <EditorProviderWrapper>
+      <div>
+        <h1>{cours.name}</h1>
+        <p>{cours.description}</p>
+      </div>
+    </EditorProviderWrapper>
+  );
 }
