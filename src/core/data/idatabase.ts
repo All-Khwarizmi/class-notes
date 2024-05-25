50e6b049e942b3c08e3492f94b30de30826c3c0d
Jason Suarez <swarecito@gmail.com>
2024-05-25 13:38:59 +0200

    feat: Add getAllCours, getSingleCours, and addCours methods to IDatabase
    This commit adds the getAllCours, getSingleCours, and addCours methods to the IDatabase interface. These methods allow for retrieving all cours for a user, retrieving a single cours by its ID, and adding a new cours, respectively. This addition enhances the functionality of the application by providing the necessary database operations for managing cours data.

--- src/core/data/idatabase.ts
+++ src/core/data/idatabase.ts
@@ -6,6 +6,7 @@ import {
   Category,
   Competence,
 } from "@/features/comp-cat/domain/entities/schemas";
+import { Cours } from "@/features/cours/domain/entities/cours-schemas";
 
 export default abstract class IDatabase {
   abstract getUser({
@@ -50,5 +51,27 @@ export default abstract class IDatabase {
     userId: string;
     category: string;
     competence: Competence;
+  }): Promise<Either<Failure<string>, void>>;
+
+  abstract getAllCours({
+    userId,
+  }: {
+    userId: string;
+  }): Promise<Either<Failure<string>, DocumentData[]>>;
+
+  abstract getSingleCours({
+    userId,
+    coursId,
+  }: {
+    userId: string;
+    coursId: string;
+  }): Promise<Either<Failure<string>, DocumentData>>;
+
+  abstract addCours({
+    userId,
+    cours,
+  }: {
+    userId: string;
+    cours: Cours;
   }): Promise<Either<Failure<string>, void>>;
 }
