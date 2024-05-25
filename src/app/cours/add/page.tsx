20048095a6bcf1de901131b97e2085eaa9eff3b1
Jason Suarez <swarecito@gmail.com>
2024-05-25 14:01:34 +0200

    feat: Add page for adding a new cours
    This commit adds a new page component for adding a new cours. The page component imports the necessary dependencies and renders the AddCoursView component, which displays a form for creating a new cours. This addition enhances the application by providing a dedicated page for adding cours and improves the organization of the codebase.

---
+++ src/app/cours/add/page.tsx
@@ -0,0 +1,5 @@
+import AddCoursView from "@/features/cours/presentation/views/AddCoursView";
+
+export default async function Page() {
+  return <AddCoursView />;
+}
---
+++ src/features/cours/presentation/views/AddCoursView.tsx
@@ -0,0 +1,7 @@
+export default async function AddCoursView() {
+  return (
+    <div>
+      <h1>Add Cours</h1>
+    </div>
+  );
+}
