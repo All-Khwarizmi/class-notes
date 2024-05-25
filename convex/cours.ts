37a714b599ac629f071b12f43a2c117f3575aaa1
Jason Suarez <swarecito@gmail.com>
2024-05-25 21:15:57 +0200

    feat: Add updateCours and updateCoursBody mutations
    This commit adds the updateCours and updateCoursBody mutations to the cours.ts file. The updateCours mutation allows users to update various properties of a course, such as the name, body, lessons, competences, description, and category. The updateCoursBody mutation specifically allows users to update the body of a course. These mutations enhance the functionality of the application by providing the ability to modify course details and content.

--- convex/cours.ts
+++ convex/cours.ts
@@ -79,3 +79,72 @@ export const getSingleCours = query({
     }
   },
 });
+
+export const updateCours = mutation({
+  args: {
+    userId: v.string(),
+    coursId: v.string(),
+    name: v.string(),
+    body: v.string(),
+    lessons: v.array(v.string()),
+    competences: v.array(v.string()),
+    description: v.string(),
+    category: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("Users")
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .first();
+
+    if (user) {
+      const existingCours = await ctx.db
+        .query("Cours")
+        .filter((q) => q.eq(q.field("_id"), args.coursId))
+        .first();
+
+      if (existingCours) {
+        const competences = await ctx.db
+          .query("Competences")
+          .filter((q) => q.eq(q.field("createdBy"), user!._id))
+          .collect();
+
+        await ctx.db.patch(existingCours._id, {
+          name: args.name,
+          body: args.body,
+          lessons: args.lessons,
+          competences: competences.map((c) => c._id),
+          description: args.description,
+          category: args.category,
+        });
+      }
+    }
+  },
+});
+
+export const updateCoursBody = mutation({
+  args: {
+    userId: v.string(),
+    coursId: v.string(),
+    body: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("Users")
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .first();
+
+    if (user) {
+      const existingCours = await ctx.db
+        .query("Cours")
+        .filter((q) => q.eq(q.field("_id"), args.coursId))
+        .first();
+
+      if (existingCours) {
+        await ctx.db.patch(existingCours._id, {
+          body: args.body,
+        });
+      }
+    }
+  },
+});
