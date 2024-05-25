4fa09ef60b2754f5e7ed94806b64549b4b661702
Jason Suarez <swarecito@gmail.com>
2024-05-25 21:03:10 +0200

    refactor: Update CoursView component to display course name and description

--- src/features/cours/presentation/views/CoursView.tsx
+++ src/features/cours/presentation/views/CoursView.tsx
@@ -1,8 +1,9 @@
-import { Edit } from "lucide-react";
+"use client"
 import { Cours } from "../../domain/entities/cours-schemas";
 import EditorProviderWrapper from "../components/EditorProvider";
+import EditorChild from "../components/EditorChild";
 
-export default async function CoursView({
+export default function CoursView({
   cours,
 }: {
   cours: Cours;
@@ -10,10 +11,8 @@ export default async function CoursView({
 }) {
   return (
     <EditorProviderWrapper>
-      <div>
-        <h1>{cours.name}</h1>
-        <p>{cours.description}</p>
-      </div>
+    <EditorChild />
     </EditorProviderWrapper>
   );
 }
+// https://www.google.com/imgres?q=image&imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1575936123452-b67c3203c357%3Fq%3D80%26w%3D1000%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%253D&imgrefurl=https%3A%2F%2Funsplash.com%2Ffr%2Fs%2Fphotos%2Fimage&docid=n9tHz9sAm2iZfM&tbnid=Ba9N09n83VlN2M&vet=12ahUKEwjk7O3OuamGAxUUcaQEHbwEC68QM3oECFUQAA..i&w=1000&h=667&hcb=2&ved=2ahUKEwjk7O3OuamGAxUUcaQEHbwEC68QM3oECFUQAA
