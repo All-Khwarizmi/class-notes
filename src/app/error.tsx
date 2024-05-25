d2f8f77e70535b1a94e0eb2e0b2e3dfa8bfd066c
Jason Suarez <swarecito@gmail.com>
2024-05-25 17:46:07 +0200

    refactor: Update import statement in cours/page.tsx
    Refactor the import statement in cours/page.tsx to import the AllCoursView component instead of the CoursView component. This change improves code clarity and consistency by using a more descriptive component name.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { PATHS } from "@/common/constants";
 /**
  * v0 by Vercel.
  * @see https://v0.dev/t/QWerxoLimky
@@ -14,12 +13,12 @@ export default function Component() {
       <div className="max-w-md px-4 py-8 sm:px-6 lg:px-8 text-center">
         <h1 className="text-4xl font-bold mb-2">Oops! Something went wrong.</h1>
         <p className="text-lg text-gray-500 dark:text-gray-400 mb-6">
-          We're sorry, but an unexpected error has occurred. Please try again
-          later.
+          We&apos;re sorry, but an unexpected error has occurred. Please try
+          again later.
         </p>
         <Link
           className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          href={`${PATHS.ROOT_PAGE()}`}
+          href={`/`}
         >
           Go back home
         </Link>
