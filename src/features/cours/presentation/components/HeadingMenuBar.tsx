15660197c9133b8180bec1a68f02f487641b8e68
Jason Suarez <swarecito@gmail.com>
2024-05-25 19:20:06 +0200

    refactor: Add HeadingMenuBar component for managing headings in the editor
    This commit adds the HeadingMenuBar component to the cours/presentation/components directory. The component provides a dropdown menu with options for setting different heading levels in the editor. It uses the @tiptap/react and @tiptap/extension-heading libraries to interact with the editor and set the appropriate heading level. This addition improves the user experience by allowing easy management of headings within the editor.

---
+++ src/features/cours/presentation/components/HeadingMenuBar.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/core/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/core/components/ui/dropdown-menu";
+import { useCurrentEditor } from "@tiptap/react";
+import { Level } from "@tiptap/extension-heading";
+
+export function HeadingMenuBar() {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  function setHeading(level: Level) {
+    editor!.chain().focus().setHeading({ level }).run();
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Paragraph</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Paragraph</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem onClick={() => setHeading(1)} className="text-4xl">
+          Heading 1
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setHeading(2)} className="text-3xl">
+          Heading 2
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setHeading(3)} className="text-2xl">
+          Heading 3
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setHeading(4)} className="text-xl">
+          Heading 4
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setHeading(5)}>
+          Heading 5
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setHeading(6)}>
+          Heading 6
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
