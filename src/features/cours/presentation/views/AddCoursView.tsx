602824accc90bc26115f6ce1da5e559704be83f2
Jason Suarez <swarecito@gmail.com>
2024-05-25 14:12:23 +0200

    feat: fetch competences and pass it to child form component

--- src/features/cours/presentation/views/AddCoursView.tsx
+++ src/features/cours/presentation/views/AddCoursView.tsx
@@ -1,7 +1,28 @@
+import { compCatUsecases } from "@/features/comp-cat/application/usecases/comp-cat-usecases";
+import AddCoursForm from "../components/AddCoursForm";
+import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
+import { isLeft } from "fp-ts/lib/Either";
+import { redirect } from "next/navigation";
+import { Competence } from "@/features/comp-cat/domain/entities/schemas";
+
 export default async function AddCoursView() {
+  const authUser = await authUseCases.getUserAuth();
+  if (isLeft(authUser)) {
+    redirect("/login");
+  }
+  const eitherCompetences = await compCatUsecases.getCompetences({
+    userId: authUser.right.userId,
+  });
+  let competences: Competence[] = [];
+  if (!isLeft(eitherCompetences)) {
+    competences = eitherCompetences.right;
+  } else {
+    competences = [];
+  }
+
   return (
     <div>
-      <h1>Add Cours</h1>
+      <AddCoursForm competences={competences} />
     </div>
   );
 }
