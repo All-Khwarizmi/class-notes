c8ec5e6f8ba9ec47f0386930cc430fd86e5a3cb0
Jason Suarez <swarecito@gmail.com>
2024-05-25 15:35:35 +0200

    Update IDatabase to return Either<Failure<string>, string> for adding a new cours

--- src/core/data/idatabase.ts
+++ src/core/data/idatabase.ts
@@ -73,5 +73,5 @@ export default abstract class IDatabase {
   }: {
     userId: string;
     cours: Omit<Cours, "_id" | "createdAt">;
-  }): Promise<Either<Failure<string>, void>>;
+  }): Promise<Either<Failure<string>, string>>;
 }
