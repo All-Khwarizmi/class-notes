70fd72fae46ca04ffdf938fb185f638e1931ae6c
Jason Suarez <swarecito@gmail.com>
2024-05-26 15:33:42 +0200

    refactor: Update AddCoursOrSequenceView to use async/await for fetching competences

--- src/features/cours-sequence/presentation/views/AddCoursView.tsx
+++ src/features/cours-sequence/presentation/views/AddCoursView.tsx
@@ -1,28 +1,89 @@
-import { compCatUsecases } from "@/features/comp-cat/application/usecases/comp-cat-usecases";
-import AddCoursForm from "../components/AddCoursForm";
-import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
-import { isLeft } from "fp-ts/lib/Either";
-import { redirect } from "next/navigation";
+"use client";
+import AddCoursOrSequenceForm from "../components/AddCoursOrSequenceForm";
 import { Competence } from "@/features/comp-cat/domain/entities/schemas";
-
-export default async function AddCoursView() {
-  const authUser = await authUseCases.getUserAuth();
-  if (isLeft(authUser)) {
-    redirect("/login");
-  }
-  const eitherCompetences = await compCatUsecases.getCompetences({
-    userId: authUser.right.userId,
+import { UserAuth } from "@/core/auth/i-auth";
+import useSaveCoursMetadata from "../../application/usecases/services/useSaveCoursMetadata";
+import { useState } from "react";
+import { Cours } from "../../domain/entities/cours-schemas";
+import { useForm } from "react-hook-form";
+import useSaveSequenceMetadata from "../../application/usecases/services/useSaveSequenceMetadata";
+export interface CoursSequenceForm
+  extends Pick<Cours, "description" | "category" | "name" | "competences"> {}
+export default function AddCoursOrSequenceView({
+  competences,
+  authUser,
+  type,
+  title
+}: {
+  competences: Competence[];
+  authUser: UserAuth;
+  type: "cours" | "sequence";
+  title: string;
+}) {
+  const [selectedCompetences, setSelectedCompetences] = useState<Competence[]>(
+    []
+  );
+  const { setSaveCoursMetadata } = useSaveCoursMetadata();
+  const { setSaveSequenceMetadata } = useSaveSequenceMetadata();
+  const [open, setOpen] = useState(false);
+  const form = useForm<CoursSequenceForm>({
+    defaultValues: {
+      description: "",
+      category: "",
+      name: "",
+      competences: [],
+    },
   });
-  let competences: Competence[] = [];
-  if (!isLeft(eitherCompetences)) {
-    competences = eitherCompetences.right;
-  } else {
-    competences = [];
+
+  function selectCompetences({
+    competence,
+    remove,
+  }: {
+    competence: Competence;
+    remove?: boolean;
+  }) {
+    if (remove) {
+      setSelectedCompetences((prev) =>
+        prev.filter((c) => c._id !== competence._id)
+      );
+    } else {
+      setSelectedCompetences((prev) => [...prev, competence]);
+    }
   }
 
+  function onSubmitCours(data: CoursSequenceForm) {
+    const newData = {
+      ...data,
+      competences: selectedCompetences.map((c) => c._id),
+    };
+    setSaveCoursMetadata({
+      cours: newData,
+      userId: authUser.userId,
+    });
+  }
+
+  function onSubmitSequence(data: CoursSequenceForm) {
+    const newData = {
+      ...data,
+      competencesIds: selectedCompetences.map((c) => c._id),
+    };
+    setSaveSequenceMetadata({
+      sequence: newData,
+      userId: authUser.userId,
+    });
+  }
   return (
     <div>
-      <AddCoursForm competences={competences} userId={authUser.right.userId} />
+      <AddCoursOrSequenceForm
+        competences={competences}
+        form={form}
+        open={open}
+        setOpen={setOpen}
+        selectedCompetences={selectedCompetences}
+        setSelectedCompetences={selectCompetences}
+        onSubmit={type === "cours" ? onSubmitCours : onSubmitSequence}
+        title={title}
+      />
     </div>
   );
 }
