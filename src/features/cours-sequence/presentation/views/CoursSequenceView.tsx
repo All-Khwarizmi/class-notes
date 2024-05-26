a3fe089f96128b57a173da1c4882c008bac2fd4f
Jason Suarez <swarecito@gmail.com>
2024-05-26 17:38:57 +0200

    refactor: Update CoursSequenceView to display sequence name and add "Add a cours" button
    This commit refactors the CoursSequenceView component in the cours-sequence feature. It updates the code to display the sequence name as a heading and adds a button labeled "Add a cours" that allows users to add a new cours to the sequence. This change improves the user experience by providing clear visual cues and an intuitive way to add cours to a sequence.

--- src/features/cours-sequence/presentation/views/CoursSequenceView.tsx
+++ src/features/cours-sequence/presentation/views/CoursSequenceView.tsx
@@ -3,6 +3,8 @@ import { Cours, Sequence } from "../../domain/entities/cours-schemas";
 import EditorProviderWrapper from "../components/EditorProvider";
 import EditorChild from "../components/EditorChild";
 import CoursSaveButton from "../components/CoursSaveButton";
+import SaveSequenceBodyButton from "../components/SaveSequenceBodyButton";
+import { Button } from "@mui/material";
 
 export default function CoursSequenceView({
   cours,
@@ -26,11 +28,27 @@ export default function CoursSequenceView({
   }
   if (type === "sequence" && sequence) {
     return (
-      <EditorProviderWrapper content={sequence.body}>
-        <EditorChild>
-          <div></div>
-        </EditorChild>
-      </EditorProviderWrapper>
+      <>
+        <h1 className="text-2xl font-bold pb-4 dark:text-slate-300 text-slate-500 ">
+          {sequence.name}
+        </h1>
+        <EditorProviderWrapper content={sequence.body}>
+          <div>
+            <SaveSequenceBodyButton userId={userId} sequence={sequence} />
+            {/* Add button to add a cours  */}
+
+            <Button
+              LinkComponent={() => <a href={`cours/add/${sequence._id}`} />}
+              className="mt-2"
+              onClick={() => {
+                console.log("Add a cours");
+              }}
+            >
+              Add a cours
+            </Button>
+          </div>
+        </EditorProviderWrapper>
+      </>
     );
   }
 
