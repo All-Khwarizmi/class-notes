061b464c81a36e37920cadaa1c621832540a0bc8
Jason Suarez <swarecito@gmail.com>
2024-05-27 23:24:37 +0200

    refactor: create an editor extions constants

---
+++ src/core/components/constants/editor-extenstions.ts
@@ -0,0 +1,35 @@
+import StarterKit from "@tiptap/starter-kit";
+import { Color } from "@tiptap/extension-color";
+import ListItem from "@tiptap/extension-list-item";
+import TextStyle from "@tiptap/extension-text-style";
+import { ResizableImage } from "mui-tiptap";
+import { TextAlign } from "@tiptap/extension-text-align";
+
+export const EXTENSIONS = [
+  TextAlign,
+  TextAlign.configure({
+    types: ["heading", "paragraph", "img"],
+    alignments: ["left", "center", "right", "justify"],
+    defaultAlignment: "left",
+  }),
+  ResizableImage,
+  ResizableImage.configure({
+    inline: true,
+    HTMLAttributes: {
+      class: "w-max-72",
+    },
+  }),
+
+  Color.configure({ types: [TextStyle.name, ListItem.name] }),
+  TextStyle,
+  StarterKit.configure({
+    bulletList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+    orderedList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+  }),
+];
