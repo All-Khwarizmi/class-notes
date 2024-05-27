3fb072c49546b11ac55154f7348b8bf51219308c
Jason Suarez <swarecito@gmail.com>
2024-05-27 23:25:04 +0200

    refactor: Update ShowSequence component to use editor extensions constants
    The ShowSequence component has been updated to use the `EXTENSIONS` constant from the editor extensions constants file. This refactor improves code readability and maintainability by centralizing the extensions configuration in a separate file.

--- src/features/cours-sequence/presentation/views/ShowSequence.tsx
+++ src/features/cours-sequence/presentation/views/ShowSequence.tsx
@@ -1,39 +1,13 @@
 "use client";
 import { EditorContent, useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import { Color } from "@tiptap/extension-color";
-import ListItem from "@tiptap/extension-list-item";
-import TextStyle from "@tiptap/extension-text-style";
-import Image from "@tiptap/extension-image";
+import { EXTENSIONS } from "@/core/components/constants/editor-extenstions";
 
 function ShowSequence({ content }: { content: string }) {
-  console.log({content});
-  const extensions = [
-  Image,
-  Image.configure({
-    inline: true,
-    HTMLAttributes: {
-      class: "w-max-72",
-    },
-  }),
-  Color.configure({ types: [TextStyle.name, ListItem.name] }),
-  TextStyle,
-  StarterKit.configure({
-    bulletList: {
-      keepMarks: true,
-      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
-    },
-    orderedList: {
-      keepMarks: true,
-      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
-    },
-  }),
-]
   const editor = useEditor({
+    editable: false,
     content,
-    extensions,
+    extensions: EXTENSIONS,
   });
-
 
   if (!editor) {
     return null;
