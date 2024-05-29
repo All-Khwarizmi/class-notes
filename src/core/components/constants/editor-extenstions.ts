6edeed795ba665fc41939fa8cd49650fe6840cd5
Jason Suarez <swarecito@gmail.com>
2024-05-29 22:42:32 +0200

    refactor: Update ResizableImage configuration in editor extensions

--- src/core/components/constants/editor-extenstions.ts
+++ src/core/components/constants/editor-extenstions.ts
@@ -2,9 +2,10 @@ import StarterKit from "@tiptap/starter-kit";
 import { Color } from "@tiptap/extension-color";
 import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
-import { ResizableImage } from "mui-tiptap";
+// import { ResizableImage } from "mui-tiptap";
 
 import { TextAlign } from "@tiptap/extension-text-align";
+import ResizableImageExtension from "../common/editor/ImageResize";
 
 export const EXTENSIONS = [
   TextAlign,
@@ -13,12 +14,16 @@ export const EXTENSIONS = [
     alignments: ["left", "center", "right", "justify"],
     defaultAlignment: "left",
   }),
-  ResizableImage,
-  ResizableImage.configure({
+  // ResizableImage,
+  // ResizableImage.configure({
+  //   inline: true,
+  //   HTMLAttributes: {},
+  // }),
+  ResizableImageExtension,
+  ResizableImageExtension.configure({
     inline: true,
     HTMLAttributes: {},
   }),
-
   Color.configure({ types: [TextStyle.name, ListItem.name] }),
   TextStyle,
   StarterKit.configure({
