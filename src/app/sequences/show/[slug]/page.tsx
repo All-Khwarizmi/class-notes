d862571acd9e47abb592ec3f4735e70255901651
Jason Suarez <swarecito@gmail.com>
2024-05-31 00:37:26 +0200

    feat: Add lazy loading for images in Cours edit page

--- src/app/sequences/show/[slug]/page.tsx
+++ src/app/sequences/show/[slug]/page.tsx
@@ -1,10 +1,7 @@
 import NotFound from "@/app/not-found";
-import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
-import { coursUsecases } from "@/features/cours-sequence/application/usecases/cours-usecases";
-import ShowSequence from "@/features/cours-sequence/presentation/views/ShowSequence";
-import { isLeft } from "fp-ts/lib/Either";
-import { redirect } from "next/navigation";
-import React from "react";
+import LoadingSkeleton from "@/core/components/common/LoadingSkeleton";
+import React, { Suspense } from "react";
+import SequenceShowServerLayer from "./SequenceShowServerLayer";
 
 async function Page({
   params,
@@ -16,37 +13,12 @@ async function Page({
   if (!params.slug) {
     return <NotFound />;
   }
-  const authUser = await authUseCases.getUserAuth();
-  if (isLeft(authUser)) {
-    redirect("/login");
-  }
 
-  const eitherSequence = await coursUsecases.getSingleSequence({
-    userId: authUser.right.userId,
-    sequenceId: params.slug,
-  });
-  if (isLeft(eitherSequence)) {
-    console.log(eitherSequence.left);
-    return <NotFound />;
-  }
-  // Get all cours from the sequence
-  const eitherCours = await coursUsecases.getAllCoursFromSequence({
-    userId: authUser.right.userId,
-    sequenceId: params.slug,
-  });
-  if (isLeft(eitherCours)) {
-    console.log(eitherCours.left);
-    return <NotFound />;
-  }
-  // Merge the sequence and cours content
-  const content =
-    eitherSequence.right.body +
-    eitherCours.right
-      .map((c) => c.body)
-      .join(" ")
-      .toString();
-
-  return <ShowSequence content={content} />;
+  return (
+    <Suspense fallback={<LoadingSkeleton />}>
+      <SequenceShowServerLayer slug={params.slug} />
+    </Suspense>
+  );
 }
 
 export default Page;
