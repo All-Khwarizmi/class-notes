b65e2a737094afb8feb67ae2d2b5fc71adc0895e
Jason Suarez <swarecito@gmail.com>
2024-05-30 21:51:31 +0200

    refactor: Implement lazy loading for CoursServerLayer component

---
+++ src/app/cours/add/[slug]/CoursAddServerLayer.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import AddCoursOrSequenceView from "@/features/cours-sequence/presentation/views/AddCoursView";
+import { compCatUsecases } from "@/features/comp-cat/application/usecases/comp-cat-usecases";
+import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
+import { isLeft } from "fp-ts/lib/Either";
+import { redirect } from "next/navigation";
+import { Competence } from "@/features/comp-cat/domain/entities/schemas";
+import NotFound from "@/app/not-found";
+
+async function CoursAddServerLayer(
+    props: { slug: string }
+) {
+     const authUser = await authUseCases.getUserAuth();
+     if (isLeft(authUser)) {
+       redirect("/login");
+     }
+
+     const eitherCompetences = await compCatUsecases.getCompetences({
+       userId: authUser.right.userId,
+     });
+
+     let competences: Competence[] = [];
+     if (!isLeft(eitherCompetences)) {
+       competences = eitherCompetences.right;
+     } else {
+       competences = [];
+     }
+  return (
+    <AddCoursOrSequenceView
+      competences={competences}
+      authUser={authUser.right}
+      sequenceId={props.slug}
+      type="cours"
+      title="Add Cours"
+    />
+  );
+}
+
+export default CoursAddServerLayer
--- src/app/cours/add/[slug]/page.tsx
+++ src/app/cours/add/[slug]/page.tsx
@@ -1,38 +1,16 @@
-import AddCoursOrSequenceView from "@/features/cours-sequence/presentation/views/AddCoursView";
-import { compCatUsecases } from "@/features/comp-cat/application/usecases/comp-cat-usecases";
-import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
-import { isLeft } from "fp-ts/lib/Either";
-import { redirect } from "next/navigation";
-import { Competence } from "@/features/comp-cat/domain/entities/schemas";
 import NotFound from "@/app/not-found";
+import { Suspense } from "react";
+import CoursAddServerLayer from "./CoursAddServerLayer";
+import LoadingSkeleton from "@/core/components/common/LoadingSkeleton";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   if (!params.slug) {
     return <NotFound />;
   }
 
-  const authUser = await authUseCases.getUserAuth();
-  if (isLeft(authUser)) {
-    redirect("/login");
-  }
-
-  const eitherCompetences = await compCatUsecases.getCompetences({
-    userId: authUser.right.userId,
-  });
-
-  let competences: Competence[] = [];
-  if (!isLeft(eitherCompetences)) {
-    competences = eitherCompetences.right;
-  } else {
-    competences = [];
-  }
   return (
-    <AddCoursOrSequenceView
-      competences={competences}
-      authUser={authUser.right}
-      sequenceId={params.slug}
-      type="cours"
-      title="Add Cours"
-    />
+    <Suspense fallback={<LoadingSkeleton />}>
+      <CoursAddServerLayer slug={params.slug} />
+    </Suspense>
   );
 }
