c9541d4d561b8b2711548b226a43c2c169e212cd
Jason Suarez <swarecito@gmail.com>
2024-05-30 21:47:15 +0200

    refactor: Update page component to use lazy loading for CoursServerLayer
    The page component in page.tsx has been refactored to improve performance by implementing lazy loading for the CoursServerLayer component. This change enhances the user experience by loading the component asynchronously, reducing the initial page load time. It also improves the maintainability of the codebase.

--- src/app/cours/[slug]/page.tsx
+++ src/app/cours/[slug]/page.tsx
@@ -1,31 +1,15 @@
 import NotFound from "@/app/not-found";
-import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
-import { coursUsecases } from "@/features/cours-sequence/application/usecases/cours-usecases";
-import CoursSequenceView from "@/features/cours-sequence/presentation/views/CoursSequenceView";
-import { isLeft } from "fp-ts/lib/Either";
-import { redirect } from "next/navigation";
+import CoursServerLayer from "./CoursServerLayer";
+import LoadingSkeleton from "@/core/components/common/LoadingSkeleton";
+import { Suspense } from "react";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   if (!params.slug) {
     return <NotFound />;
   }
-  const authUser = await authUseCases.getUserAuth();
-  if (isLeft(authUser)) {
-    redirect("/login");
-  }
-  const eitherCours = await coursUsecases.getSingleCours({
-    userId: authUser.right.userId,
-    coursId: params.slug,
-  });
-  if (isLeft(eitherCours)) {
-    return <NotFound />;
-  }
-  const cours = eitherCours.right;
   return (
-    <CoursSequenceView
-      cours={cours}
-      userId={authUser.right.userId}
-      type="cours"
-    />
+    <Suspense fallback={<LoadingSkeleton />}>
+      <CoursServerLayer slug={params.slug} />
+    </Suspense>
   );
 }
