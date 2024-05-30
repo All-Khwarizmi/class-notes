388c763df122be800de2c2de248611690257c111
Jason Suarez <swarecito@gmail.com>
2024-05-30 22:09:09 +0200

    refactor: Update CoursSaveButton component
    The CoursSaveButton component in CoursSaveButton.tsx has been updated to use the AfterMenuButton component and the lucide-react library for improved UI in the editor. This change enhances the editor experience by providing a button with a save icon, allowing users to save the cours body. It also improves the maintainability of the codebase.

--- src/core/components/common/editor/CoursSaveButton.tsx
+++ src/core/components/common/editor/CoursSaveButton.tsx
@@ -1,8 +1,9 @@
-import { Button } from "@/core/components/ui/button";
 import React from "react";
 import useUpdateCoursBody from "../../../../features/cours-sequence/application/usecases/services/useUpdateCoursBody";
 import { Cours } from "../../../../features/cours-sequence/domain/entities/cours-schemas";
 import { useCurrentEditor } from "@tiptap/react";
+import AfterMenuButton from "./AfterMenuButton";
+import { Save } from "lucide-react";
 
 function CoursSaveButton({ cours, userId }: { cours: Cours; userId: string }) {
   const { setUpdateCoursBodyOptions } = useUpdateCoursBody();
@@ -11,18 +12,19 @@ function CoursSaveButton({ cours, userId }: { cours: Cours; userId: string }) {
     return null;
   }
   return (
-    <Button
-      className="mt-2"
-      onClick={() => {
-        setUpdateCoursBodyOptions({
-          userId,
-          coursId: cours._id,
-          body: editor.getHTML(),
-        });
+    <AfterMenuButton
+      props={{
+        onClick: () => {
+          setUpdateCoursBodyOptions({
+            userId,
+            coursId: cours._id,
+            body: editor.getHTML(),
+          });
+        },
       }}
     >
-      Save
-    </Button>
+      <Save size={12} />
+    </AfterMenuButton>
   );
 }
 
