0db3bcaba07835e0dcd70eaa8444ffe2cf31e66c
Jason Suarez <swarecito@gmail.com>
2024-05-31 17:04:38 +0200

    feat: Add API methods for managing CoursComplement

--- src/features/cours-complement/application/repositories/cours-complement-repository.ts
+++ src/features/cours-complement/application/repositories/cours-complement-repository.ts
@@ -1,9 +1,36 @@
 import IDatabase from "@/core/data/idatabase";
+import { CoursComplement } from "../../domain/cours-complement-schemas";
 
 export default class CoursComplementRepository {
   private readonly _db: IDatabase;
 
   constructor(db: IDatabase) {
     this._db = db;
+  }
+
+  async addCoursComplement({
+    userId,
+    coursComplement,
+  }: {
+    userId: string;
+    coursComplement: Omit<CoursComplement, "_id" | "createdAt">;
+  }) {
+    return this._db.addCoursComplement({ coursComplement, userId });
+  }
+
+  async getAllCoursComplement({ coursId }: { coursId: string }) {
+    return this._db.getAllCoursComplement({ coursId });
+  }
+
+  async getCoursComplement({ id }: { id: string }) {
+    return this._db.getCoursComplement({ id });
+  }
+
+  async updateCoursComplement({
+    coursComplement,
+  }: {
+    coursComplement: CoursComplement;
+  }) {
+    return this._db.updateCoursComplement({ coursComplement });
   }
 }
