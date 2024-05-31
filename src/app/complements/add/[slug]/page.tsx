8c4b61cce50911d167236736d796e74fac6c7b37
Jason Suarez <swarecito@gmail.com>
2024-05-31 20:58:25 +0200

    refactor: Add lazy loading for ComplementAddBaseForm component
    This commit adds lazy loading for the ComplementAddBaseForm component in the page.tsx file. The Suspense component is used to wrap the form component and a LoadingSkeleton component is shown as a fallback while the form is loading. This enhancement improves the page load performance by only loading the form component when it is needed.

--- src/app/complements/add/[slug]/page.tsx
+++ src/app/complements/add/[slug]/page.tsx
@@ -1,8 +1,13 @@
+import LoadingSkeleton from "@/core/components/common/LoadingSkeleton";
 import ComplementAddBaseForm from "@/features/complement/presentation/views/ComplementAddBase";
-import React from "react";
+import React, { Suspense } from "react";
 
 function Page({ params }: { params: { slug: string } }) {
-  return <ComplementAddBaseForm slug={params.slug} />;
+  return (
+    <Suspense fallback={<LoadingSkeleton />}>
+      <ComplementAddBaseForm slug={params.slug} />
+    </Suspense>
+  );
 }
 
 export default Page;
