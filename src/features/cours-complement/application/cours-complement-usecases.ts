8ffc05fc494e79e7b00c62f216863e125c9e036d
Jason Suarez <swarecito@gmail.com>
2024-05-31 17:06:59 +0200

    feat: Add CoursComplementUsecases class
    Add the CoursComplementUsecases class to the cours-complement application. This class provides methods for adding, retrieving, updating, and deleting CoursComplement entities. It utilizes the CoursComplementRepository for data persistence. This change expands the functionality of the codebase and aligns with recent user commits for managing CoursComplement entities.

---
+++ src/features/cours-complement/application/cours-complement-usecases.ts
@@ -0,0 +1,36 @@
+import { CoursComplement } from "../domain/cours-complement-schemas";
+import CoursComplementRepository from "./repositories/cours-complement-repository";
+
+export default class CoursComplementUsecases {
+  private readonly _repository: CoursComplementRepository;
+
+  constructor({ repository }: { repository: CoursComplementRepository }) {
+    this._repository = repository;
+  }
+
+    async addCoursComplement({
+        userId,
+        coursComplement,
+    }: {
+        userId: string;
+        coursComplement: Omit<CoursComplement, "_id" | "createdAt">;
+    }) {
+        return this._repository.addCoursComplement({ userId, coursComplement });
+    }
+
+    async getAllCoursComplement({ coursId }: { coursId: string }) {
+        return this._repository.getAllCoursComplement({ coursId });
+    }
+
+    async getCoursComplement({ id }: { id: string }) {
+        return this._repository.getCoursComplement({ id });
+    }
+
+    async updateCoursComplement({
+        coursComplement,
+    }: {
+        coursComplement: CoursComplement;
+    }) {
+        return this._repository.updateCoursComplement({ coursComplement });
+    }
+}
