6103bd67ed45789b25264eb8fa43bcdf11dfd938
Jason Suarez <swarecito@gmail.com>
2024-06-01 02:12:20 +0200

    refactor: Update ComplementView to include AfterMenuBar component
    The ComplementView component has been updated to include the AfterMenuBar component, which contains the UpdateComplement component. This refactor enhances the modularity and reusability of the codebase, aligning with recent user commits and improving the overall user experience.

--- src/features/complement/presentation/views/ComplementView.tsx
+++ src/features/complement/presentation/views/ComplementView.tsx
@@ -1,12 +1,22 @@
+"use client";
 import Editor from "@/core/components/common/editor/Editor";
 import React from "react";
 import { Complement } from "../../domain/complement-schemas";
+import AfterMenuBar from "@/core/components/common/editor/AfterMunuBar";
+import UpdateComplement from "../components/UpdateComplement";
 
 function ComplementView(props: { slug: string; complement: Complement }) {
   return (
-    <Editor content={props.complement.body}>
-      <div>Complement id : {props.slug}</div>
-    </Editor>
+    <Editor
+      content={props.complement.body}
+      slotafter={
+        <div className=" flex flex-col gap-4 ">
+          <AfterMenuBar>
+            <UpdateComplement complement={props.complement} />
+          </AfterMenuBar>
+        </div>
+      }
+    />
   );
 }
 
