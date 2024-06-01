3c25518920b9d54bcd78d2cc966e12da964762a4
Jason Suarez <swarecito@gmail.com>
2024-06-01 02:12:01 +0200

    refactor: Update Editor component to include placeholder content

--- src/core/components/common/editor/Editor.tsx
+++ src/core/components/common/editor/Editor.tsx
@@ -12,20 +12,7 @@ function Editor(props: {
         props.content !== ""
           ? props.content
           : `
-<div class="placeholder-content">
-<p style="text-align: center"><span class="react-renderer node-image ProseMirror-selectednode" contenteditable="false" draggable="true"><div as="div" draggable="true" data-drag-handle="true" data-node-view-wrapper="" style="white-space: normal; overflow: hidden; position: relative; display: inline-block; line-height: 0px;"><div style="overflow: hidden; position: relative; display: inline-block; line-height: 0px;"><img src="https://assets-global.website-files.com/645a9acecda2e0594fac6126/6580a563d237ee85c9237ccb_gradient-noise-purple-azure.png" alt="Placeholder Image" width="327" height="auto style="cursor: default;"></div></div></span><img class="ProseMirror-separator" alt=""><br class="ProseMirror-trailingBreak"></p>
-<h2 style="text-align: center">🔥🔥 Welcome to Your Editor</h2>
-  <p>To get started, try the following:</p>
-  <ul>
-    <li><strong>Bold</strong> your text by selecting it and pressing <code>Ctrl+B</code>.</li>
-    <li><em>Italicize</em> your text by selecting it and pressing <code>Ctrl+I</code>.</li>
-    <li>Add <a href="#">links</a> to enrich your content.</li>
-  </ul>
-  <blockquote>
-    <p>This is a blockquote. Use it to highlight important information or quotes.</p>
-  </blockquote>
-  <p>Start typing to explore more features!</p>
-</div>
+  '<p style="text-align: center"></p><p style="text-align: center"><img src="https://assets-global.website-files.com/645a9acecda2e0594fac6126/6580a563d237ee85c9237ccb_gradient-noise-purple-azure.png" alt="Placeholder Image" width="327" height="auto style="></p><p><br></p><p></p><h2 style="text-align: center">🔥🔥 Welcome to Your Editor</h2><p style="text-align: center"></p><h4>To get started, try the following:</h4><ul><li><p><strong>Bold</strong> your text by selecting it and pressing <code>Ctrl+B</code>.</p></li><li><p><em>Italicize</em> your text by selecting it and pressing <code>Ctrl+I</code>.</p></li><li><p>Add links to enrich your content.</p></li></ul><blockquote><h4>This is a blockquote. Use it to highlight important information or quotes.</h4></blockquote><h4>Start typing to explore more features!</h4>'
           `
       }
     >
