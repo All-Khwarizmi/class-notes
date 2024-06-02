e0a69747152b44fc68fa6cfeda7d182f381395de
Jason Suarez <swarecito@gmail.com>
2024-06-02 02:13:59 +0200

    refactor: Update createNote mutation to include type and contentType fields
    The createNote mutation in the notes.ts file has been updated to include the type and contentType fields. This change allows for specifying the type of note (Folder or Item) and the content type (Diagram, Flowchart, or Markup) when creating a new note. It enhances the functionality of the createNote mutation and provides more flexibility in note creation.

--- convex/notes.ts
+++ convex/notes.ts
@@ -29,6 +29,12 @@ export const createNote = mutation({
     createdBy: v.string(),
     keywords: v.array(v.string()),
     content: v.string(),
+    type: v.union(v.literal("Folder"), v.literal("Item")),
+    contentType: v.union(
+      v.literal("Diagram"),
+      v.literal("Flowchart"),
+      v.literal("Markup")
+    ),
   },
   handler: async (ctx, args) => {
     const categoryId = await ctx.db.insert("Notes", {
@@ -41,6 +47,8 @@ export const createNote = mutation({
       pathDictionary: args.pathDictionary,
       folders: args.folders,
       keywords: args.keywords,
+      type: args.type,
+      contentType: args.contentType,
     });
     return categoryId;
   },
@@ -73,54 +81,54 @@ export const getNote = query({
 });
 
 export const updateNote = mutation({
-    args: {
-        id: v.string(),
-        name: v.optional(v.string()),
-        description: v.optional(v.string()),
-        content: v.optional(v.string()),
-        fullPath: v.optional(v.string()),
-        pathDictionary: v.optional(
-            v.array(
-                v.object({
-                    id: v.string(),
-                    name: v.string(),
-                })
-            )
-        ),
-        folders: v.optional(
-            v.array(
-                v.object({
-                    id: v.string(),
-                    name: v.string(),
-                    contentType: v.union(
-                        v.literal("Diagram"),
-                        v.literal("Flowchart"),
-                        v.literal("Markup")
-                    ),
-                    createdAt: v.number(),
-                })
-            )
-        ),
-        keywords: v.optional(v.array(v.string())),
-    },
-    handler: async (ctx, args) => {
-        const note = await ctx.db
-            .query("Notes")
-            .filter((q) => q.eq(q.field("_id"), args.id))
-            .first();
-        if (note) {
-            await ctx.db.patch(note?._id, {
-                name: args.name,
-                description: args.description,
-                content: args.content,
-                fullPath: args.fullPath,
-                pathDictionary: args.pathDictionary,
-                folders: args.folders,
-                keywords: args.keywords,
-            });
-            return true;
-        } else {
-            return false;
-        }
-    },
+  args: {
+    id: v.string(),
+    name: v.optional(v.string()),
+    description: v.optional(v.string()),
+    content: v.optional(v.string()),
+    fullPath: v.optional(v.string()),
+    pathDictionary: v.optional(
+      v.array(
+        v.object({
+          id: v.string(),
+          name: v.string(),
+        })
+      )
+    ),
+    folders: v.optional(
+      v.array(
+        v.object({
+          id: v.string(),
+          name: v.string(),
+          contentType: v.union(
+            v.literal("Diagram"),
+            v.literal("Flowchart"),
+            v.literal("Markup")
+          ),
+          createdAt: v.number(),
+        })
+      )
+    ),
+    keywords: v.optional(v.array(v.string())),
+  },
+  handler: async (ctx, args) => {
+    const note = await ctx.db
+      .query("Notes")
+      .filter((q) => q.eq(q.field("_id"), args.id))
+      .first();
+    if (note) {
+      await ctx.db.patch(note?._id, {
+        name: args.name,
+        description: args.description,
+        content: args.content,
+        fullPath: args.fullPath,
+        pathDictionary: args.pathDictionary,
+        folders: args.folders,
+        keywords: args.keywords,
+      });
+      return true;
+    } else {
+      return false;
+    }
+  },
 });
