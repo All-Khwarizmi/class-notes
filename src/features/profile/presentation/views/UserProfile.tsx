2940db88ac7ed1cd432b15472642f1914bb11b41
Jason Suarez <swarecito@gmail.com>
2024-06-04 07:50:56 +0200

    Fix tests

--- src/features/profile/presentation/views/UserProfile.tsx
+++ src/features/profile/presentation/views/UserProfile.tsx
@@ -17,11 +17,7 @@ import {
 import { Input } from "@/core/components/ui/input";
 import useSaveUser from "../../application/adapters/services/useSaveUser";
 export default function UserProfile({ user }: { user: UserType }) {
-  const {
-    setSaveUserOptions,
-    loading,
-    error,
-  } = useSaveUser()
+  const { setSaveUserOptions, loading, error } = useSaveUser();
   const form = useForm<UserType>({
     resolver: zodResolver(
       userSchema.omit({
@@ -32,16 +28,15 @@ export default function UserProfile({ user }: { user: UserType }) {
   });
 
   function onSubmit(data: UserType) {
-    console.log({ data })
+    console.log({ data });
     setSaveUserOptions({
       userId: user._id,
       ...data,
-    
     });
   }
 
   return (
-    <div className="py-8 px-6">
+    <div data-testid="user-form" className="py-8 px-6">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
           <FormField
@@ -52,7 +47,11 @@ export default function UserProfile({ user }: { user: UserType }) {
                 <FormItem>
                   <FormLabel htmlFor={field.name}>Name</FormLabel>
                   <FormControl>
-                    <Input {...field} placeholder="John Doe" />
+                    <Input
+                      data-testid="name-input"
+                      {...field}
+                      placeholder="John Doe"
+                    />
                   </FormControl>
                   <FormDescription>Enter your full name</FormDescription>
                 </FormItem>
@@ -67,7 +66,11 @@ export default function UserProfile({ user }: { user: UserType }) {
                 <FormItem>
                   <FormLabel htmlFor={field.name}>School Subject</FormLabel>
                   <FormControl>
-                    <Input {...field} placeholder="Math" />
+                    <Input
+                      data-testid="matiere-input"
+                      {...field}
+                      placeholder="Math"
+                    />
                   </FormControl>
                   <FormDescription>Enter your school subject</FormDescription>
                 </FormItem>
@@ -75,6 +78,7 @@ export default function UserProfile({ user }: { user: UserType }) {
             }}
           />
           <button
+            data-testid="submit-onboarding-form"
             type="submit"
             className="bg-blue-500 text-white px-4 py-2 rounded"
           >
