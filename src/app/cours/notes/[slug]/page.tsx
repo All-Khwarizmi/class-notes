e325e8f110aac0e3d7390b3a7465156dec4256fd
Jason Suarez <swarecito@gmail.com>
2024-06-05 16:56:36 +0200

    Add cours notes path

---
+++ src/app/cours/notes/[slug]/page.tsx
@@ -0,0 +1,19 @@
+import React, { Suspense } from "react";
+import LoadingSkeleton from "@/core/components/common/LoadingSkeleton";
+import NotesServerLayer from "@/app/profile/notes/[slug]/NotesServerLayer";
+
+async function Page({
+  params,
+}: {
+  params: {
+    slug: string;
+  };
+}) {
+  return (
+    <Suspense fallback={<LoadingSkeleton />}>
+      <NotesServerLayer slug={params.slug} type="cours" />
+    </Suspense>
+  );
+}
+
+export default Page;
