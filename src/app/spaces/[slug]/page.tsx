62533134fcac578ef07ad4015fc3016f6764b751
Jason Suarez <swarecito@gmail.com>
2024-06-08 01:18:21 +0200

    refactor: Simplify code in page component

--- src/app/spaces/[slug]/page.tsx
+++ src/app/spaces/[slug]/page.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import UserSpace from "./UserSpace";
 
 async function Page({ params }: { params: { slug: string } }) {
-  return <UserSpace slug={params.slug} />;
+  return(
+    
+    <UserSpace slug={params.slug} />);
 }
 
 export default Page;
