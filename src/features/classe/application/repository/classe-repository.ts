81474f355d751884df3c36e415994acbdf72bac7
Jason Suarez <swarecito@gmail.com>
2024-06-07 23:44:57 +0200

    create classe usecase services folder and move hooks to it

--- src/features/classe/application/repository/classe-repository.ts
+++ src/features/classe/application/repository/classe-repository.ts
@@ -2,10 +2,10 @@ import useCreateClasseInfra from "@/features/classe/infra/services/useCreateClas
 import useDeleteClasseInfra from "@/features/classe/infra/services/useDeleteClasseInfra";
 import useGetClasseInfra from "@/features/classe/infra/services/useGetClasseInfra";
 import useGetClassesInfra from "@/features/classe/infra/services/useGetClassesInfra";
-import useCreateClasseUsecase from "../usecases/useCreateClasseUsecase";
-import useDeleteClasseUsecase from "../usecases/useDeleteClasseUsecase";
-import useGetClasseUsecase from "../usecases/useGetClasseUsecase";
-import useGetClassesUsecase from "../usecases/useGetClassesUsecase";
+import useCreateClasseUsecase from "../usecases/services/useCreateClasseUsecase";
+import useDeleteClasseUsecase from "../usecases/services/useDeleteClasseUsecase";
+import useGetClasseUsecase from "../usecases/services/useGetClasseUsecase";
+import useGetClassesUsecase from "../usecases/services/useGetClassesUsecase";
 import authRepository from "@/features/auth/application/repository/old-auth-repository";
 
 export const classeRepository = {
---
+++ src/features/classe/application/usecases/services/useUpdateClasseVisibility.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+function useUpdateClasseVisibility() {}
+
+export default useUpdateClasseVisibility;
