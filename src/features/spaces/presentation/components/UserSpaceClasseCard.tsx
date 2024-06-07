1402691ae8e8527efd3e75e83ee2dc32afc4477e
Jason Suarez <swarecito@gmail.com>
2024-06-08 00:49:03 +0200

    feat: Add UserSpaceClasseCard component

---
+++ src/features/spaces/presentation/components/UserSpaceClasseCard.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/core/components/ui/button";
+import { ClassType } from "@/features/classe/domain/class-schema";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+
+function UserSpaceClasseCard(props: { classe: ClassType }) {
+  return (
+    <div
+      className={cn(
+        "bg-white rounded-lg overflow-hidden shadow-md dark:bg-gray-950 dark:text-gray-200 border ",
+        `hover:shadow-lg hover:scale-105 transition duration-300 ease-in-out shadow-slate-800 dark:border-gray-800 dark:hover:shadow-slate-900 dark:hover:border-gray-900`
+      )}
+    >
+      <img
+        src={
+          props.classe.imageUrl ?? "/images/mos-design-jzFbbG2WXv0-unsplash.jpg"
+        }
+        alt="Card 1"
+        width={400}
+        height={300}
+        className="w-full h-48 object-cover"
+      />
+      <div className="p-4">
+        <h3 className="text-lg font-semibold mb-2">{props.classe.name}</h3>
+        <p className="text-gray-500 dark:text-gray-400 mb-4">
+          {props.classe.description}
+        </p>
+        <Button variant="outline" className="w-full">
+          View Classe
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default UserSpaceClasseCard;
