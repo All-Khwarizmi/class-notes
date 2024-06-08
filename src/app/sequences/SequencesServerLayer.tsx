22dda0384dee2a253df5b25cb8be9fb0f9b18c5b
Jason Suarez <swarecito@gmail.com>
2024-06-08 20:32:05 +0200

    refactor: Update SequencesServerLayer to handle error when fetching sequences

--- src/app/sequences/SequencesServerLayer.tsx
+++ src/app/sequences/SequencesServerLayer.tsx
@@ -3,9 +3,9 @@ import { authUseCases } from "@/features/auth/application/usecases/auth-usecases
 import { coursUsecases } from "@/features/cours-sequence/application/usecases/cours-usecases";
 import { isLeft } from "fp-ts/lib/Either";
 import { redirect } from "next/navigation";
-import NotFound from "../not-found";
 import SequencesListView from "@/features/cours-sequence/presentation/views/SequencesListView";
 import Sidebar from "@/core/components/layout/Sidebar";
+import ErrorDialog from "@/core/components/common/ErrorDialog";
 
 async function SequencesServerLayer() {
   const authUser = await authUseCases.getUserAuth();
@@ -16,7 +16,15 @@ async function SequencesServerLayer() {
     userId: authUser.right.userId,
   });
   if (isLeft(eitherSequences)) {
-    return <NotFound />;
+    return (
+      <ErrorDialog
+        message={`
+      An error occurred while fetching sequences. 
+    `}
+        code={eitherSequences.left.code}
+        description={eitherSequences.left.message}
+      />
+    );
   }
   return (
     <>
