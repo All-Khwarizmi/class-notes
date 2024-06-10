57d6face977a853a5c662ae902cb8e106d073f74
Jason Suarez <swarecito@gmail.com>
2024-06-10 23:09:00 +0200

    refactor: Update ComplementView to include eye button for viewing complement details

--- src/features/complement/presentation/views/ComplementView.tsx
+++ src/features/complement/presentation/views/ComplementView.tsx
@@ -4,8 +4,15 @@ import React from "react";
 import { Complement } from "../../domain/complement-schemas";
 import AfterMenuBar from "@/core/components/common/editor/AfterMunuBar";
 import UpdateComplement from "../components/UpdateComplement";
+import { Eye } from "lucide-react";
+import Link from "next/link";
+import AfterMenuButton from "@/core/components/common/editor/AfterMenuButton";
 
-function ComplementView(props: { slug: string; complement: Complement }) {
+function ComplementView(props: {
+  slug: string;
+  complement: Complement;
+  userId: string;
+}) {
   //! Check complement type and return the right component
   return (
     <Editor
@@ -14,6 +21,13 @@ function ComplementView(props: { slug: string; complement: Complement }) {
         <div className=" flex flex-col gap-4 ">
           <AfterMenuBar>
             <UpdateComplement complement={props.complement} />
+            <AfterMenuButton>
+              <Link
+                href={`/spaces/complement/${props.complement.id}?user=${props.userId}`}
+              >
+                <Eye size={12} />
+              </Link>
+            </AfterMenuButton>
           </AfterMenuBar>
         </div>
       }
