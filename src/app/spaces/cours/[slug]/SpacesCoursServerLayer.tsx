b653f78d80786d344d6453ba9ac5b74b396f0ec1
Jason Suarez <swarecito@gmail.com>
2024-06-10 22:46:50 +0200

    refactor: spaces cours to have url params and fetch visibility table to be able to display content acordingly

--- src/app/spaces/cours/[slug]/SpacesCoursServerLayer.tsx
+++ src/app/spaces/cours/[slug]/SpacesCoursServerLayer.tsx
@@ -1,15 +1,30 @@
 import ErrorDialog from "@/core/components/common/ErrorDialog";
 import Sidebar from "@/core/components/layout/Sidebar";
 import SpacesHeader from "@/core/components/layout/SpacesHeader";
+import getVisibility from "@/features/classe/application/adapters/actions/get-visibility";
 import { complementUsecases } from "@/features/complement/application/usecases/complement-usecases";
 import { Complement } from "@/features/complement/domain/complement-schemas";
 import { coursUsecases } from "@/features/cours-sequence/application/usecases/cours-usecases";
 import ShowSequence from "@/features/cours-sequence/presentation/views/ShowSequence";
 import { isLeft } from "fp-ts/lib/Either";
 import { ClipboardType, ScrollText } from "lucide-react";
+import Error from "next/error";
 import React from "react";
 
-async function SpacesCoursServerLayer(props: { slug: string }) {
+async function SpacesCoursServerLayer(props: {
+  slug: string;
+  searchParams: { [key: string]: string | undefined };
+}) {
+  if (!props.slug || !props.searchParams.user) {
+    return (
+      <ErrorDialog
+        message={`
+        The user id or the course id is missing
+        `}
+      />
+    );
+  }
+  const userId = props.searchParams.user;
   const eitherCours = await coursUsecases.getSingleCours({
     userId: "",
     coursId: props.slug,
@@ -26,7 +41,21 @@ async function SpacesCoursServerLayer(props: { slug: string }) {
       />
     );
   }
-
+  const eitherVibility = await getVisibility({ userId });
+  if (isLeft(eitherVibility)) {
+    console.log(eitherVibility.left);
+    return (
+      <ErrorDialog
+        message="An error occured while fetching the course visibility"
+        code={eitherVibility.left.code}
+        description={
+          process.env.NODE_ENV === "development"
+            ? eitherVibility.left.message
+            : ""
+        }
+      />
+    );
+  }
   const eitherComplements = await complementUsecases.getAllCoursComplement({
     coursId: props.slug,
   });
@@ -47,7 +76,13 @@ async function SpacesCoursServerLayer(props: { slug: string }) {
   const complements: Complement[] = [];
 
   for (const complement of eitherComplements.right) {
-    if (complement.publish === true) {
+    const visSibilityComplement = eitherVibility.right.complement.find(
+      (vis) => vis.id === complement.id
+    );
+    if (!visSibilityComplement) {
+      continue;
+    }
+    if (visSibilityComplement.publish === true) {
       complements.push(complement);
     }
   }
--- src/app/spaces/cours/[slug]/page.tsx
+++ src/app/spaces/cours/[slug]/page.tsx
@@ -2,10 +2,16 @@ import LoadingSkeleton from "@/core/components/common/LoadingSkeleton";
 import React, { Suspense } from "react";
 import SpacesCoursServerLayer from "./SpacesCoursServerLayer";
 
-function Page({ params }: { params: { slug: string } }) {
+function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | undefined };
+}) {
   return (
     <Suspense fallback={<LoadingSkeleton />}>
-      <SpacesCoursServerLayer slug={params.slug} />
+      <SpacesCoursServerLayer slug={params.slug} searchParams={searchParams} />
     </Suspense>
   );
 }
