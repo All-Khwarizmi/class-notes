4de915adc279ded6dea8324e7a60357123c994db
Jason Suarez <swarecito@gmail.com>
2024-06-10 23:13:51 +0200

    refactor: Update ComplementView to include VisibilitySwitch for controlling complement visibility

--- src/features/complement/presentation/views/ComplementView.tsx
+++ src/features/complement/presentation/views/ComplementView.tsx
@@ -7,6 +7,7 @@ import UpdateComplement from "../components/UpdateComplement";
 import { Eye } from "lucide-react";
 import Link from "next/link";
 import AfterMenuButton from "@/core/components/common/editor/AfterMenuButton";
+import VisibilitySwitch from "@/features/cours-sequence/presentation/components/VisibilitySwitch";
 
 function ComplementView(props: {
   slug: string;
@@ -20,14 +21,23 @@ function ComplementView(props: {
       slotafter={
         <div className=" flex flex-col gap-4 ">
           <AfterMenuBar>
-            <UpdateComplement complement={props.complement} />
-            <AfterMenuButton>
-              <Link
-                href={`/spaces/complement/${props.complement.id}?user=${props.userId}`}
-              >
-                <Eye size={12} />
-              </Link>
-            </AfterMenuButton>
+            <div className="flex items-center justify-between w-full gap-4 ">
+              <div className="flex items-center gap-1">
+                <UpdateComplement complement={props.complement} />
+                <AfterMenuButton>
+                  <Link
+                    href={`/spaces/complement/${props.complement.id}?user=${props.userId}`}
+                  >
+                    <Eye size={12} />
+                  </Link>
+                </AfterMenuButton>
+              </div>
+              <VisibilitySwitch
+                userId={props.userId}
+                type="complement"
+                typeId={props.complement.id}
+              />
+            </div>
           </AfterMenuBar>
         </div>
       }
