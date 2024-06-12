71a8ff364adf12807c0aaae42a32a4336800f1ee
Jason Suarez <swarecito@gmail.com>
2024-06-12 23:36:35 +0200

    refactor: Remove unused code and improve user authentication handling in UserProfile component

--- src/features/profile/presentation/views/UserProfile.tsx
+++ src/features/profile/presentation/views/UserProfile.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import {
   UserType,
   userSchema,
@@ -14,16 +13,12 @@ import {
   FormItem,
   FormLabel,
 } from "@/core/components/ui/form";
-import { useAuthStore } from "@/core/auth/auth-store";
-
 import { Input } from "@/core/components/ui/input";
 import useSaveUser from "../../application/adapters/services/useSaveUser";
-import { UserButton, SignInButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/core/components/ui/button";
+
 export default function UserProfile({ user }: { user: UserType }) {
-  const { isLoggedIn } = useAuthStore((state) => ({
-    isLoggedIn: state.isLoggedIn,
-  }));
   const { setSaveUserOptions, loading, error } = useSaveUser();
   const form = useForm<UserType>({
     resolver: zodResolver(
@@ -44,13 +39,7 @@ export default function UserProfile({ user }: { user: UserType }) {
   return (
     <div data-testid="user-form" className="py-8 px-6">
       <div className="flex justify-end gap-4 pb-4">
-        {isLoggedIn() ? (
-          <UserButton />
-        ) : (
-          <SignInButton>
-            <Button>Se Connecter</Button>
-          </SignInButton>
-        )}
+        <UserButton />
       </div>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
