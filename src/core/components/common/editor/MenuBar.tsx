e24de9a22fb83574b4071a430b38e309823d8a6c
Jason Suarez <swarecito@gmail.com>
2024-06-14 00:52:44 +0200

    refactor: Remove unused code from MenuBar component

--- src/core/components/common/editor/MenuBar.tsx
+++ src/core/components/common/editor/MenuBar.tsx
@@ -6,9 +6,6 @@ import {
   Italic,
   List,
   ListOrdered,
-  Menu,
-  Minus,
-  Plus,
   Quote,
   Redo,
   Strikethrough,
@@ -23,7 +20,6 @@ import MenuImage from "./MenuImage";
 import MenuButton from "@/core/components/common/editor/MenuButton";
 import MenuTextAlign from "@/core/components/common/editor/MenuTextAlign";
 import { cn } from "@/lib/utils";
-import AfterMenuButton from "./AfterMenuButton";
 
 export const MenuBar = () => {
   const { editor } = useCurrentEditor();
@@ -138,86 +134,6 @@ export const MenuBar = () => {
       >
         <Video size={12} />
       </MenuButton>
-
-      {/* <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
-      >
-        h1
-      </button>
-
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
-      >
-        h2
-      </button>
-
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
-      >
-        h3
-      </button> */}
-
-      {/* <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={!editor.can().chain().focus().toggleStrike().run()}
-        className={editor.isActive("strike") ? "is-active" : ""}
-      >
-        strike
-      </button> */}
-
-      {/* <button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        disabled={!editor.can().chain().focus().toggleCode().run()}
-        className={editor.isActive("code") ? "is-active" : ""}
-      >
-        code
-      </button> */}
-
-      {/* <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-        clear marks
-      </button> */}
-
-      {/* <button onClick={() => editor.chain().focus().clearNodes().run()}>
-        clear nodes
-      </button> */}
-
-      {/* <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive("codeBlock") ? "is-active" : ""}
-      >
-        code block
-      </button> */}
-
-      {/* <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
-      >
-        h4
-      </button> */}
-
-      {/* <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
-      >
-        h5
-      </button> */}
-
-      {/* <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
-      >
-        h6
-      </button> */}
-
-      {/* <button
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive("paragraph") ? "is-active" : ""}
-      >
-        paragraph
-      </button> */}
     </div>
   );
 };
