1c5ea77622e6c0703abff9907ddf5143781bf087
Jason Suarez <swarecito@gmail.com>
2024-06-14 18:48:23 +0200

    feat: Refactor getEvaluationBase method to return validated evaluation data
    Refactor the getEvaluationBase method in the EvaluationUsecases class to return the validated evaluation data instead of the raw data. This improves data integrity and ensures that only valid evaluation bases are returned. The method now returns a Promise that resolves to either a Failure object or the validated evaluation data.

--- src/features/evaluation/application/usecases/evaluation-usecases.ts
+++ src/features/evaluation/application/usecases/evaluation-usecases.ts
@@ -1,3 +1,4 @@
+import { Either, isLeft, left, right } from "fp-ts/lib/Either";
 import {
   CreateEvaluationOptions,
   GetEvaluationBaseOptions,
@@ -7,6 +8,12 @@ import {
 import EvaluatioRepository, {
   evaluatioRepository,
 } from "../repositories/evaluation-repository";
+import {
+  EvaluationBaseSchema,
+  EvaluationBaseType,
+} from "../../domain/entities/evaluation-schema";
+import Failure from "@/core/failures/failures";
+import { id } from "fp-ts/lib/Refinement";
 
 export default class EvaluationUsecases {
   private readonly _evaluationRepository: EvaluatioRepository;
@@ -19,8 +26,31 @@ export default class EvaluationUsecases {
     return await this._evaluationRepository.createEvaluation(options);
   }
 
-  async getEvaluationBase(options: GetEvaluationBaseOptions) {
-    return await this._evaluationRepository.getEvaluationBase(options);
+  async getEvaluationBase(
+    options: GetEvaluationBaseOptions
+  ): Promise<Either<Failure<string>, EvaluationBaseType>> {
+    const eitherEval = await this._evaluationRepository.getEvaluationBase(
+      options
+    );
+    if (isLeft(eitherEval)) {
+      return eitherEval;
+    }
+
+    const validatedEval = EvaluationBaseSchema.safeParse({
+      ...eitherEval.right,
+      id: eitherEval.right._id,
+    });
+    if (!validatedEval.success) {
+      return left(
+        Failure.invalidValue({
+          message: "Invalid Evaluation Base",
+          invalidValue: eitherEval.right,
+          code: "APP203",
+        })
+      );
+    }
+
+    return right(validatedEval.data);
   }
 
   async getEvaluationBases(options: GetEvaluationBasesOptions) {
