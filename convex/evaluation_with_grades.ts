8c2864813aa5f12c830193d4378e320d0572cf33
Jason Suarez <swarecito@gmail.com>
2024-06-15 21:06:41 +0200

    refactor: Update imports for grade type modules
    This commit updates the imports for the grade type modules in the codebase. It renames the import paths from "../fields/grade-type" to "../fields/grade_type" in multiple files, including api.d.ts, evaluation-base.ts, and evaluations_with_grades.ts. This update ensures that the correct modules are imported and improves the consistency of the codebase.

--- convex/evaluation_with_grades.ts
+++ convex/evaluation_with_grades.ts
@@ -1,34 +1,49 @@
+import {
+  Grade,
+  StudentGradeType,
+} from "@/features/evaluation/domain/entities/evaluation-with-grades-schema";
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
-import { gradeType } from "./fields/grade_type";
 
-export const createEvaluationWithGrade = mutation({
+export const assignEvaluationToClasse = mutation({
   args: {
-    evaluationDate: v.float64(),
     classeId: v.string(),
-    evaluationBaseId: v.string(),
-    grades: v.array(
-      v.object({
-        studentId: v.string(),
-        feedback: v.string(),
-        grades: v.array(
-          v.object({
-            criteriaId: v.string(),
-            gradeType,
-            grade: v.union(v.number(), v.string()),
-          })
-        ),
-      })
-    ),
+    evaluationId: v.string(),
   },
   handler: async (ctx, args) => {
+    const evaluationBase = await ctx.db
+      .query("EvaluationBase")
+      .filter((q) => q.eq(q.field("_id"), args.evaluationId))
+      .first();
+
+    if (!evaluationBase) {
+      throw new Error("Evaluation base not found");
+    }
+    const students = await ctx.db
+      .query("Students")
+      .filter((q) => q.eq(q.field("classId"), args.classeId))
+      .collect();
+
+    if (!students) {
+      throw new Error("Students not found");
+    }
+    // Create default grades for each student
+    const grades: StudentGradeType[] = students.map((student) => ({
+      studentId: student._id,
+      feedback: "",
+      grades: evaluationBase.criterias.map((criteria) => ({
+        criteriaId: criteria.id,
+        gradeType: criteria.gradeType,
+        grade: "",
+      })),
+    }));
     // Create the evaluation with grades
     const resultId = await ctx.db.insert("EvaluationsWithGrades", {
       publishDate: Date.now(),
-      evaluationDate: args.evaluationDate,
+      evaluationDate: Date.now(),
       classeId: args.classeId,
-      evaluationBaseId: args.evaluationBaseId,
-      grades: args.grades,
+      evaluationBaseId: args.evaluationId,
+      grades,
     });
 
     if (!resultId) {
@@ -36,6 +51,47 @@ export const createEvaluationWithGrade = mutation({
     }
 
     return resultId;
+  },
+});
+
+export const updateGrade = mutation({
+  args: {
+    evaluationId: v.string(),
+    studentId: v.string(),
+    criteriaId: v.string(),
+    grade: v.union(v.number(), v.string()),
+  },
+  handler: async (ctx, args) => {
+    const evaluation = await ctx.db
+      .query("EvaluationsWithGrades")
+      .filter((q) => q.eq(q.field("_id"), args.evaluationId))
+      .first();
+
+    if (!evaluation) {
+      throw new Error("Evaluation not found");
+    }
+
+    const studentGrade = evaluation.grades.find(
+      (grade) => grade.studentId === args.studentId
+    );
+
+    if (!studentGrade) {
+      throw new Error("Student not found");
+    }
+
+    const criteriaGrade = studentGrade.grades.find(
+      (grade) => grade.criteriaId === args.criteriaId
+    );
+
+    if (!criteriaGrade) {
+      throw new Error("Criteria not found");
+    }
+
+    criteriaGrade.grade = args.grade;
+
+    await ctx.db.patch(evaluation._id, evaluation);
+
+    return evaluation;
   },
 });
 
