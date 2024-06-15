9272fb8845dfdc5e32c3e24285f4868ee461354e
Jason Suarez <swarecito@gmail.com>
2024-06-15 21:29:53 +0200

    feat: Add getClassNavItems utility function
    This commit adds the getClassNavItems utility function to the get-classe-nav-items.tsx file. This function generates an array of navigation items for a specific class, including sequences, evaluations, and notes. It enhances the functionality of the application by providing a convenient way to generate class navigation items dynamically.

---
+++ src/features/evaluation/application/adapters/utils/get-classe-nav-items.tsx
@@ -0,0 +1,52 @@
+import Dashboard from "@/core/components/icons/Dashboard";
+import { Sequence } from "@/features/cours-sequence/domain/entities/cours-schemas";
+import { NavItem } from "@/lib/types";
+import {
+  Presentation,
+  Plus,
+  CandlestickChart,
+  NotebookPen,
+} from "lucide-react";
+
+export default function getClassNavItems(options: {
+  sequences: Sequence[];
+  classeId: string;
+}) {
+  const sequenceNavItems: NavItem[] = options.sequences.map((sequence) => ({
+    title: sequence.name,
+    href: `/sequences/${sequence._id}?type=sequence`,
+    icon: <Presentation size={16} />,
+  }));
+
+  sequenceNavItems.push({
+    title: "Add new sequence",
+    href: `/classes/sequences/${options.classeId}`,
+    icon: <Plus size={16} />,
+  });
+  const classeNavItems: NavItem[] = [
+    {
+      title: "Dashboard",
+      href: "/dashboard",
+      icon: Dashboard(),
+    },
+    {
+      title: "Sequences",
+      href: `#`,
+      icon: <Presentation size={16} />,
+      isChidren: true,
+      children: sequenceNavItems,
+    },
+    {
+      title: "Evaluations",
+      href: `/evaluations/assign/${options.classeId}`,
+      icon: <CandlestickChart size={16} />,
+    },
+    {
+      title: "Notes",
+      href: `/classes/notes/${options.classeId}`,
+      icon: <NotebookPen size={16} />,
+    },
+  ];
+
+  return classeNavItems;
+}
