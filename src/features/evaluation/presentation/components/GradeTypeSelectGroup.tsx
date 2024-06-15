f91657afb950f03d63e03a96ae2e3463afd4d966
Jason Suarez <swarecito@gmail.com>
2024-06-15 15:50:57 +0200

    feat: Add GradeTypeSelectGroup component
    This commit adds the GradeTypeSelectGroup component to the evaluation presentation components directory. The component provides a select group with various grade type options for users to choose from. This addition enhances the functionality of the application by allowing users to select the appropriate grade type for evaluations.

---
+++ src/features/evaluation/presentation/components/GradeTypeSelectGroup.tsx
@@ -0,0 +1,47 @@
+import {
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+} from "@/core/components/ui/select";
+import React from "react";
+
+function GradeTypeSelectGroup() {
+  return (
+    <SelectGroup>
+      <SelectLabel>Numeric</SelectLabel>
+      <SelectItem value="Numeric">Numeric</SelectItem>
+      <SelectItem value="Percentage">Percentage</SelectItem>
+      <SelectItem value="A/B/C/D/F">US Letter Grades</SelectItem>
+      <SelectItem value="A/B/C/D/F/Pass/Fail">
+        US Letter Grades with Pass/Fail
+      </SelectItem>
+      <SelectItem value="A/B/C/D/F/Pass/Fail/None">
+        US Letter Grades with Pass/Fail/None
+      </SelectItem>
+      <SelectItem value="Pass/Fail">Pass/Fail</SelectItem>
+      <SelectItem value="Excellent/Good/Satisfactory/Needs Improvement">
+        Descriptive Grades
+      </SelectItem>
+      <SelectItem value="4.0 Scale">US 4.0 Scale</SelectItem>
+      <SelectItem value="First/Upper Second/Lower Second/Third">
+        UK Honors
+      </SelectItem>
+      <SelectItem value="10-point Scale">10-point Scale</SelectItem>
+      <SelectItem value="20-point Scale">20-point Scale</SelectItem>
+      <SelectItem value="Grade Points">Grade Points</SelectItem>
+      <SelectItem value="Other">Custom</SelectItem>
+      <SelectLabel>Sports</SelectLabel>
+      <SelectItem value="Points">Sport Points</SelectItem>
+      <SelectItem value="Ranking">Sport Ranking</SelectItem>
+      <SelectItem value="Win/Loss/Tie">Sport Result</SelectItem>
+      <SelectItem value="Performance Level">Sport Performance</SelectItem>
+      <SelectLabel>International</SelectLabel>
+      <SelectItem value="French Grading">French Grading</SelectItem>
+      <SelectItem value="German Grading">German Grading</SelectItem>
+      <SelectItem value="Australian Grading">Australian Grading</SelectItem>
+      <SelectItem value="Spanish Grading">Spanish Grading</SelectItem>
+    </SelectGroup>
+  );
+}
+
+export default GradeTypeSelectGroup;
