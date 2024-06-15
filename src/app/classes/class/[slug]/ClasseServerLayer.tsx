28fd1463a411e5d6cfbcb15fbcce34a5d7f155fa
Jason Suarez <swarecito@gmail.com>
2024-06-15 23:55:30 +0200

    Fetch needed data to display classe table

--- src/app/classes/class/[slug]/ClasseServerLayer.tsx
+++ src/app/classes/class/[slug]/ClasseServerLayer.tsx
@@ -2,9 +2,13 @@ import ErrorDialog from "@/core/components/common/ErrorDialog";
 import Dashboard from "@/core/components/icons/Dashboard";
 import Sidebar from "@/core/components/layout/Sidebar";
 import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
+import getStudents from "@/features/classe/application/adapters/actions/get-students";
+import { ClasseTableType } from "@/features/classe/domain/class-schema";
+import { StudentsEvaluationTableView } from "@/features/classe/presentation/components/ClasseTable";
 import StudentsTable from "@/features/classe/presentation/components/StudentsTable";
 import { coursUsecases } from "@/features/cours-sequence/application/usecases/cours-usecases";
 import getClassNavItems from "@/features/evaluation/application/adapters/utils/get-classe-nav-items";
+import { evaluationUsecases } from "@/features/evaluation/application/usecases/evaluation-usecases";
 import { NavItem } from "@/lib/types";
 import { isLeft } from "fp-ts/lib/Either";
 import {
@@ -34,16 +38,49 @@ async function ClasseServerLayer(props: { slug: string }) {
     );
   }
 
+  const eitherCompoundEvaluations = await evaluationUsecases.getEvaluationsList(
+    {
+      classeId: props.slug,
+    }
+  );
+
+  if (isLeft(eitherCompoundEvaluations)) {
+    return (
+      <ErrorDialog
+        message="An error occurred"
+        description="An error occurred while fetching evaluations"
+        code={eitherCompoundEvaluations.left.code}
+      />
+    );
+  }
+
+  const eitherStudents = await getStudents({ classeId: props.slug });
+  if (isLeft(eitherStudents)) {
+    return (
+      <ErrorDialog
+        message="An error occurred"
+        description="An error occurred while fetching students"
+        code={eitherStudents.left.code}
+      />
+    );
+  }
+
   const classeNavItems = getClassNavItems({
     sequences: eitherSequences.right,
     classeId: props.slug,
   });
+
+  const tableData: ClasseTableType = {
+    students: eitherStudents.right,
+    evaluations: eitherCompoundEvaluations.right,
+  };
   return (
     <>
       <Sidebar navItems={classeNavItems} />
       <section className="h-full flex-1  overflow-x-hidden">
         <div className="h-full pt-4 px-4">
-          <StudentsTable classId={props.slug} />
+          {/* <StudentsTable classId={props.slug} /> */}
+          <StudentsEvaluationTableView {...tableData} />
         </div>
       </section>
     </>
