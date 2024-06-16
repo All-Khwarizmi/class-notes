64aaa8d27edf78e1375c9d7a3fadc2da4fb59f90
Jason Suarez <swarecito@gmail.com>
2024-06-16 19:01:22 +0200

    refactor: Add checkSpecialGradeType utility function
    A new utility function, checkSpecialGradeType, has been added to the project. This function checks if a given grade is one of the special grade types defined in the SpecialGradeTypes union. It returns an object with a boolean shouldReturn property indicating whether the grade is a special type or not, and a returnValue property containing the grade value. This addition enhances the flexibility and accuracy of grade type checking in the evaluation application.

---
+++ src/features/evaluation/application/adapters/utils/checkSpecialGradeType.ts
@@ -0,0 +1,47 @@
+import { z, ZodLiteral } from "zod";
+
+// Define the specific new grade types
+const SpecialGradeTypes = z.union([
+  z.literal("N/G"), // Not Graded
+  z.literal("M"), // Missing
+  z.literal("N/D"), // Not Done
+  z.literal("N/A"), // Not Applicable
+  z.literal("Error"), // Error
+]);
+
+export type SpecialGradeType = z.infer<typeof SpecialGradeTypes>;
+type NormalTypes = number | string;
+
+// Define the union of the new grade types
+type GradeTypeUnion =
+  | { shouldReturn: true; returnValue: SpecialGradeType }
+  | { shouldReturn: false; returnValue: NormalTypes };
+// Define the type for the return object
+
+
+// Helper function to check if the grade is one of the special types
+function checkSpecialGradeType(grade: unknown): GradeTypeUnion {
+  const result = SpecialGradeTypes.safeParse(grade);
+
+  if (result.success) {
+    return {
+      shouldReturn: true,
+      returnValue: result.data,
+    };
+  } else {
+    const result = z.union([z.number(), z.string()]).safeParse(grade);
+    if (result.success) {
+      return {
+        shouldReturn: false,
+        returnValue: result.data,
+      };
+    } else {
+      return {
+        shouldReturn: true,
+        returnValue: "N/A",
+      };
+    }
+  }
+}
+
+export default checkSpecialGradeType;
