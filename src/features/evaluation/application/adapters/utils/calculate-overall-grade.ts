67b76783a1f684912a71497158afb55c79fc5253
Jason Suarez <swarecito@gmail.com>
2024-06-16 17:55:53 +0200

    refactor: Update calculateOverallGrade function to support different grade types
    The calculateOverallGrade function in calculate-overall-grade.ts has been updated to support different grade types. It now accepts an object with grades, criteria, and gradeType properties, allowing for more flexibility in calculating the overall grade. This change improves the functionality of the function and enhances its usability in various evaluation scenarios.

--- src/features/evaluation/application/adapters/utils/calculate-overall-grade.ts
+++ src/features/evaluation/application/adapters/utils/calculate-overall-grade.ts
@@ -1,15 +1,64 @@
 import { Grade } from "@/features/evaluation/domain/entities/evaluation-with-grades-schema";
-
+import {
+  NumericGradeType,
+  NumericGradeSchema,
+  EvaluationCriteriaType,
+  GradeTypeUnionType,
+  TenPointScaleSchema,
+  TenPointScaleType,
+} from "@/features/evaluation/domain/entities/evaluation-schema";
 // Calculate the overall grade for a student in an evaluation
-export default function calculateOverallGrade(grades: Grade[]) {
+export default function calculateOverallGrade({
+  grades,
+  criteria,
+  gradeType,
+}: {
+  grades: Grade[];
+  criteria: EvaluationCriteriaType[];
+  gradeType: GradeTypeUnionType;
+}) {
   if (!grades || grades.length === 0) return "N/A";
-  const numericGrades = grades
-    .map((grade) => (typeof grade.grade === "number" ? grade.grade : null))
-    .filter(Boolean) as number[];
-  if (numericGrades.length > 0) {
-    return (
-      numericGrades.reduce((a, b) => a + b, 0) / numericGrades.length
-    ).toFixed(2);
+
+  // Switch between the different types of grades
+  switch (gradeType.type) {
+    case "10-point Scale":
+      const totalWeight = criteria.reduce(
+        (acc, criteria) => acc + criteria.weight,
+        0
+      );
+      let totalPoints: number = 0;
+      for (const grade of grades) {
+        const validatedGrade = TenPointScaleSchema.safeParse(grade.gradeType);
+        if (validatedGrade.success) {
+          const points = tenPointsScaleCalc({
+            grade: validatedGrade.data,
+            criterias: criteria,
+          });
+          if (points === "N/A") {
+            return "N/A";
+          }
+          totalPoints += points;
+        } else {
+          return "Error";
+        }
+      }
+      return (totalPoints / totalWeight) * 10;
+    default:
+      return "N/A";
   }
-  return "N/A";
+
+}
+
+function tenPointsScaleCalc({
+  grade,
+  criterias,
+}: {
+  grade: TenPointScaleType;
+  criterias: EvaluationCriteriaType[];
+}) {
+  const criteria = criterias.find((criteria) => criteria.id === grade.name);
+  if (!criteria) return "N/A";
+
+  // Calculate the overall grade based on the criteria
+  return grade.grade;
 }
