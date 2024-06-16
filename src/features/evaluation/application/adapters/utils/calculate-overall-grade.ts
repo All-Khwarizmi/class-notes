b8bd20e3e3068201ac563bc88d70a603f4015c04
Jason Suarez <swarecito@gmail.com>
2024-06-16 19:01:30 +0200

    refactor: Update calculateOverallGrade function to support different grade types

--- src/features/evaluation/application/adapters/utils/calculate-overall-grade.ts
+++ src/features/evaluation/application/adapters/utils/calculate-overall-grade.ts
@@ -7,6 +7,20 @@ import {
   TenPointScaleSchema,
   TenPointScaleType,
 } from "@/features/evaluation/domain/entities/evaluation-schema";
+import checkSpecialGradeType, {
+  SpecialGradeType,
+} from "./checkSpecialGradeType";
+import { z } from "zod";
+import { tenPointsScaleCase } from "./ten-points-scale-case";
+
+export type SwitchReturnType =
+  | string
+  | number
+  | "N/G"
+  | "M"
+  | "N/D"
+  | "N/A"
+  | "Error";
 // Calculate the overall grade for a student in an evaluation
 export default function calculateOverallGrade({
   grades,
@@ -16,49 +30,14 @@ export default function calculateOverallGrade({
   grades: Grade[];
   criteria: EvaluationCriteriaType[];
   gradeType: GradeTypeUnionType;
-}) {
+}): SwitchReturnType {
   if (!grades || grades.length === 0) return "N/A";
 
   // Switch between the different types of grades
   switch (gradeType.type) {
     case "10-point Scale":
-      const totalWeight = criteria.reduce(
-        (acc, criteria) => acc + criteria.weight,
-        0
-      );
-      let totalPoints: number = 0;
-      for (const grade of grades) {
-        const validatedGrade = TenPointScaleSchema.safeParse(grade.gradeType);
-        if (validatedGrade.success) {
-          const points = tenPointsScaleCalc({
-            grade: validatedGrade.data,
-            criterias: criteria,
-          });
-          if (points === "N/A") {
-            return "N/A";
-          }
-          totalPoints += points;
-        } else {
-          return "Error";
-        }
-      }
-      return (totalPoints / totalWeight) * 10;
+      return tenPointsScaleCase(grades, criteria);
     default:
       return "N/A";
   }
-
-}
-
-function tenPointsScaleCalc({
-  grade,
-  criterias,
-}: {
-  grade: TenPointScaleType;
-  criterias: EvaluationCriteriaType[];
-}) {
-  const criteria = criterias.find((criteria) => criteria.id === grade.name);
-  if (!criteria) return "N/A";
-
-  // Calculate the overall grade based on the criteria
-  return grade.grade;
 }
