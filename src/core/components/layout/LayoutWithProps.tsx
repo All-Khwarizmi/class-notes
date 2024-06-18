ca432e461dc7bdb6d492fd04352336f50ca7a8b3
Jason Suarez <swarecito@gmail.com>
2024-06-18 20:04:55 +0200

    refactor: Update LayoutWithProps component to conditionally render NotFound and NothingToShow components
    This commit updates the LayoutWithProps component in the LayoutWithProps.tsx file to include conditional rendering of the NotFound and NothingToShow components. If the notFound prop is true, the NotFound component will be rendered. If the nothingToShow prop is true, the NothingToShow component will be rendered. This change enhances the flexibility of the component and improves the handling of not found pages and empty content scenarios.

--- src/core/components/layout/LayoutWithProps.tsx
+++ src/core/components/layout/LayoutWithProps.tsx
@@ -4,16 +4,19 @@ import { NavItem } from "@/lib/types";
 import Sidebar from "./Sidebar";
 import { not } from "fp-ts/lib/Predicate";
 import NotFound from "@/app/not-found";
+import NothingToShow from "../common/editor/NothingToShow";
 function LayoutWithProps({
   children,
   navItems,
   isEmpty = false,
   notFound = false,
+  nothingToShow = false,
 }: Readonly<{
   children?: React.ReactNode;
   navItems?: NavItem[];
   isEmpty?: boolean;
   notFound?: boolean;
+  nothingToShow?: boolean;
 }>) {
   return (
     <>
@@ -21,7 +24,13 @@ function LayoutWithProps({
       <section className="flex h-full w-full border-collapse overflow-hidden">
         <Sidebar navItems={isEmpty === true ? [] : navItems} />
         <section className="h-full flex-1  pt-4 px-4 overflow-x-hidden">
-          {notFound === true ? <NotFound /> : <>{children}</>}
+          {notFound === true ? (
+            <NotFound />
+          ) : nothingToShow === true ? (
+            <NothingToShow />
+          ) : (
+            children
+          )}
         </section>
       </section>
     </>
