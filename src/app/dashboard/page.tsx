2f8b164f0488fe6771db6237d34ffd453a1230cc
Jason Suarez <swarecito@gmail.com>
2024-06-18 22:32:42 +0200

    refactor: Update Page component to use lazy loading for DashboardServerLayer
    This commit updates the Page component in the page.tsx file to use lazy loading for the DashboardServerLayer component. It replaces the CustomDialog and OnboardingForm components with the Suspense and LoadingSkeleton components, respectively. This change improves the page load performance by loading the DashboardServerLayer component lazily, reducing the initial load time for the Dashboard page.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,46 +1,11 @@
-"use client";
-import CustomDialog from "@/core/components/common/CustomDialog";
-import OnboardingForm from "./OboardingForm";
-import { useEffect, useState } from "react";
-import { useAuthStore } from "@/core/auth/auth-store";
-import useAuth from "@/core/auth/useAuth";
-import { userRepositry } from "@/features/user/application/repository/user-repository";
+import { Suspense } from "react";
+import LoadingSkeleton from "@/core/components/common/LoadingSkeleton";
+import DashboardServerLayer from "./DashboardServerLayer";
 
 export default function Page() {
-  const [open, setOpen] = useState(false);
-  useAuth();
-  const { onboarding, user, setOnboarding } = useAuthStore((state) => ({
-    onboarding: state.onboarding,
-    user: state.user,
-    setOnboarding: state.setOnboarding,
-  }));
-  const { user: userFromDb, isLoading } = userRepositry.useGetUser();
-
-  useEffect(() => {
-    console.log({ user, onboarding });
-    if (!user || !onboarding) {
-      setOpen(true);
-    } else {
-      setOpen(false);
-    }
-  }, [user, onboarding]);
-
-  useEffect(() => {
-    if (!isLoading && !userFromDb) {
-      setOnboarding(false);
-    }
-  }, [userFromDb, isLoading]);
   return (
-    <section data-testid="dashboard">
-      <h1>Dashboard</h1>
-      <CustomDialog
-        open={open}
-        title={"Bienvenue !"}
-        icon={undefined}
-        displayButton={false}
-      >
-        <OnboardingForm />
-      </CustomDialog>
-    </section>
+    <Suspense fallback={<LoadingSkeleton />}>
+      <DashboardServerLayer />
+    </Suspense>
   );
 }
