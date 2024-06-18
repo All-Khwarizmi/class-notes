22baf61adcb97cc050fcb67c5162d73c674e2b7b
Jason Suarez <swarecito@gmail.com>
2024-06-18 19:42:23 +0200

    refactor: Update ClasseSequencesServerLayer to use LayoutWithProps for improved layout customization

--- src/app/classes/sequences/[slug]/ClasseSequencesServerLayer.tsx
+++ src/app/classes/sequences/[slug]/ClasseSequencesServerLayer.tsx
@@ -6,6 +6,8 @@ import { redirect } from "next/navigation";
 import Sidebar from "@/core/components/layout/Sidebar";
 import ErrorDialog from "@/core/components/common/ErrorDialog";
 import ClasseSequencesTableView from "@/features/cours-sequence/presentation/views/ClasseSequencesTableView";
+import { Layout } from "lucide-react";
+import LayoutWithProps from "@/core/components/layout/LayoutWithProps";
 
 async function ClasseSequencesServerLayer(props: { slug: string }) {
   const authUser = await authUseCases.getUserAuth();
@@ -17,26 +19,23 @@ async function ClasseSequencesServerLayer(props: { slug: string }) {
   });
   if (isLeft(eitherSequences)) {
     return (
-      <ErrorDialog
-        message="An error occurred"
-        description="An error occurred while fetching sequences"
-        code={eitherSequences.left.code}
-      />
+      <LayoutWithProps isEmpty>
+        <ErrorDialog
+          message="An error occurred"
+          description="An error occurred while fetching sequences"
+          code={eitherSequences.left.code}
+        />
+      </LayoutWithProps>
     );
   }
 
   return (
-    <>
-      <Sidebar />
-      <section className="h-full flex-1  overflow-x-hidden">
-        <div className="h-full pt-4 px-6">
-          <ClasseSequencesTableView
-            sequences={eitherSequences.right}
-            classeId={props.slug}
-          />
-        </div>
-      </section>
-    </>
+    <LayoutWithProps>
+      <ClasseSequencesTableView
+        sequences={eitherSequences.right}
+        classeId={props.slug}
+      />
+    </LayoutWithProps>
   );
 }
 
