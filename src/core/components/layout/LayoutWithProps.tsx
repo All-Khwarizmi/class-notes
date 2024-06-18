98df28e8692c9e4b58c182c8a835934317dbb2d1
Jason Suarez <swarecito@gmail.com>
2024-06-18 20:02:52 +0200

    refactor: Update LayoutWithProps component to conditionally render NotFound component
    This commit updates the LayoutWithProps component in the LayoutWithProps.tsx file. It adds a new prop called notFound, which allows for conditional rendering of the NotFound component. If the notFound prop is true, the NotFound component will be rendered instead of the children. This change enhances the flexibility of the component and improves the handling of not found pages.

--- src/core/components/layout/LayoutWithProps.tsx
+++ src/core/components/layout/LayoutWithProps.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import Header from "@/core/components/layout/Header";
 import { NavItem } from "@/lib/types";
 import Sidebar from "./Sidebar";
+import { not } from "fp-ts/lib/Predicate";
+import NotFound from "@/app/not-found";
 function LayoutWithProps({
   children,
   navItems,
   isEmpty = false,
+  notFound = false,
 }: Readonly<{
-  children: React.ReactNode;
+  children?: React.ReactNode;
   navItems?: NavItem[];
   isEmpty?: boolean;
+  notFound?: boolean;
 }>) {
   return (
     <>
@@ -17,7 +21,7 @@ function LayoutWithProps({
       <section className="flex h-full w-full border-collapse overflow-hidden">
         <Sidebar navItems={isEmpty === true ? [] : navItems} />
         <section className="h-full flex-1  pt-4 px-4 overflow-x-hidden">
-          {children}
+          {notFound === true ? <NotFound /> : <>{children}</>}
         </section>
       </section>
     </>
