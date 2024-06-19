9b40f602a23fdc9a637f0d2fce225dc2a1a2c44a
Jason Suarez <swarecito@gmail.com>
2024-06-19 21:03:02 +0200

    refactor: Add FloatingEditor component for improved editing experience
    This commit adds the FloatingEditor component, which provides an enhanced editing experience. The component utilizes the @tiptap/react package and includes features such as a floating menu and editor content. This addition improves the usability and functionality of the editor in the application.

---
+++ src/core/components/common/editor/FloatingEditor.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import { EditorContent, FloatingMenu, useEditor } from "@tiptap/react";
+import { EXTENSIONS } from "@/core/components/constants/editor-extenstions";
+import { DebouncedFunc } from "lodash";
+import FloatingMenuBar from "@/core/components/common/editor/FloatingMenuBar";
+
+function FloatingEditor(props: {
+  content: string;
+  debounceUpdateFn: DebouncedFunc<(content: string) => void>;
+}) {
+  const editor = useEditor({
+    extensions: EXTENSIONS,
+    content: props.content,
+    onUpdate: (editor) => {
+      const content = editor.editor.getHTML();
+      props.debounceUpdateFn(content);
+    },
+  });
+
+  return (
+    <>
+      {editor && (
+        <>
+          <FloatingMenu editor={editor}>
+            <FloatingMenuBar editor={editor} />
+          </FloatingMenu>
+          <EditorContent editor={editor} />
+        </>
+      )}
+    </>
+  );
+}
+
+export default FloatingEditor;
