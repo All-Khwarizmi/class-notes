b0f2827114a22d0c67347a673bfa3d17caf5f08c
Jason Suarez <swarecito@gmail.com>
2024-06-19 22:10:21 +0200

    ```text refactor: Update FloatingEditor to use placeholder content when props.content is empty
    This commit updates the FloatingEditor component to use a placeholder content when the props.content is empty. The placeholder content includes instructions and examples to help users get started with the editor. This change improves the user experience by providing clear guidance and reducing confusion when there is no initial content.

--- src/core/components/common/editor/FloatingEditor.tsx
+++ src/core/components/common/editor/FloatingEditor.tsx
@@ -12,9 +12,13 @@ function FloatingEditor(props: {
   afterMenuBar?: boolean;
   children?: React.ReactNode;
 }) {
+  const contentOrPlaceholder =
+    props.content !== ""
+      ? props.content
+      : '<p style="text-align: center"></p><p style="text-align: center"><img src="https://assets-global.website-files.com/645a9acecda2e0594fac6126/6580a563d237ee85c9237ccb_gradient-noise-purple-azure.png" alt="Placeholder Image" width="327" height="auto style="></p><p><br></p><p></p><h2 style="text-align: center">🔥🔥 Welcome to Your Editor</h2><p style="text-align: center"></p><h4>To get started, try the following:</h4><ul><li><p><strong>Bold</strong> your text by selecting it and pressing <code>Ctrl+B</code>.</p></li><li><p><em>Italicize</em> your text by selecting it and pressing <code>Ctrl+I</code>.</p></li><li><p>Add links to enrich your content.</p></li></ul><blockquote><h4>This is a blockquote. Use it to highlight important information or quotes.</h4></blockquote><h4>Start typing to explore more features!</h4>';
   const editor = useEditor({
     extensions: EXTENSIONS,
-    content: props.content,
+    content: contentOrPlaceholder,
     onUpdate: (editor) => {
       const content = editor.editor.getHTML();
       props.debounceUpdateFn(content);
@@ -31,9 +35,7 @@ function FloatingEditor(props: {
           <EditorContent editor={editor} />
           {props.afterMenuBar && (
             <div className="flex flex-col gap-4 ">
-              <AfterMenuBar
-                editor={editor}
-              >{props.children}</AfterMenuBar>
+              <AfterMenuBar editor={editor}>{props.children}</AfterMenuBar>
             </div>
           )}
         </>
