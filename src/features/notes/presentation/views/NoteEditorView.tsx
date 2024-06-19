59e578e8ad030a208f170eb3e433ff6e2e0d2c1f
Jason Suarez <swarecito@gmail.com>
2024-06-19 15:41:19 +0200

    refactor: Update NoteEditorView to use @tiptap/react for editor functionality
    This commit updates the NoteEditorView component to use the @tiptap/react package for the editor functionality. It replaces the previous Editor component with the useEditor hook from @tiptap/react, allowing for improved code organization and consistency within the project.

--- src/features/notes/presentation/views/NoteEditorView.tsx
+++ src/features/notes/presentation/views/NoteEditorView.tsx
@@ -6,24 +6,38 @@ import UpdateNoteButton from "../components/UpdateNote";
 import useUpdateNote from "../../application/adapters/services/useUpdateNote";
 import { cn } from "@/lib/utils";
 import AfterMenuBar from "@/core/components/common/editor/AfterMunuBar";
+import { EditorContent, FloatingMenu, useEditor } from "@tiptap/react";
+import { EXTENSIONS } from "@/core/components/constants/editor-extenstions";
+import { debounce } from "lodash";
+import FloatingMenuBar from "@/core/components/common/editor/FloatingMenuBar";
 
 function NoteEditorView(props: { note: Note }) {
   const { setNote } = useUpdateNote();
+  const debouncedUpdate = debounce((content: string) => {
+    setNote({
+      ...props.note,
+      content: content,
+    });
+  }, 10000);
+  const editor = useEditor({
+    extensions: EXTENSIONS,
+    content: props.note.content,
+    onUpdate: (editor) => {
+      const content = editor.editor.getHTML();
+      debouncedUpdate(content);
+    },
+  });
   return (
-    <Editor
-      onUpdate={(content) => {
-        setNote({
-          ...props.note,
-          content: content,
-        });
-      }}
-      content={props.note.content}
-      slotafter={
-        <AfterMenuBar>
-          <UpdateNoteButton note={props.note} />
-        </AfterMenuBar>
-      }
-    />
+    <>
+      {editor && (
+        <>
+          <FloatingMenu editor={editor}>
+            <FloatingMenuBar editor={editor} />
+          </FloatingMenu>
+          <EditorContent editor={editor} />
+        </>
+      )}
+    </>
   );
 }
 
