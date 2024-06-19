2cb2c5cf38ca059a5ec875da727308e3476db6db
Jason Suarez <swarecito@gmail.com>
2024-06-19 19:52:19 +0200

    refactor: Update MenuTextColor component to use Popover for color picker
    This commit refactors the MenuTextColor component to use the Popover component for the color picker functionality. The previous implementation used a DropdownMenu component, but now it has been replaced with the Popover component from the @/core/components/ui/popover module. This change enhances the user experience by providing a more modern and intuitive color picker interface.

--- src/core/components/common/editor/MenuTextColor.tsx
+++ src/core/components/common/editor/MenuTextColor.tsx
@@ -1,42 +1,40 @@
-import { useCurrentEditor } from "@tiptap/react";
+import { Editor } from "@tiptap/react";
 import { HexColorPicker } from "react-colorful";
+
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/core/components/ui/dropdown-menu";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/core/components/ui/popover";
+
 import { useState } from "react";
 import { Pipette } from "lucide-react";
-export default function MenuTextColor() {
+export default function MenuTextColor(props: { editor: Editor }) {
+  const editor = props.editor;
   const [color, setColor] = useState("#aabbcc");
-  const { editor } = useCurrentEditor();
-  if (!editor) {
-    return null;
-  }
 
   function setTextColor(color: string) {
     editor!.chain().focus().setColor(color).run();
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <Popover>
+      <PopoverTrigger asChild>
         <button className="bg-slate-400 rounded-md p-1 px-2">
           <Pipette size={12} />
         </button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>Text color</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <div className="flex items-center gap-2">
-          <HexColorPicker
-            color={color}
-            onChange={(color) => setTextColor(color)}
-          />
-        </div>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </PopoverTrigger>
+      <PopoverContent className="w-full">
+        <HexColorPicker color={color} onChange={setColor} />
+        <button
+          className="bg-slate-400 flex justify-center rounded-md p-1 px-2 mt-2 w-full"
+          onClick={() => {
+            setTextColor(color);
+          }}
+        >
+          <Pipette size={12} />
+        </button>
+      </PopoverContent>
+    </Popover>
   );
 }
