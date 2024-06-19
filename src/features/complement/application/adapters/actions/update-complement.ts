1e3920a09c637bcc533e0595203e2f94bfa6318d
Jason Suarez <swarecito@gmail.com>
2024-06-19 22:27:03 +0200

    feat: Add updateComplement function to update course complement
    This commit adds the updateComplement function, which is responsible for updating the course complement. The function takes in the options object containing the complement data and calls the updateCoursComplement function from the complement usecases. This feature enhances the functionality of the application by allowing users to update the course complement easily.

---
+++ src/features/complement/application/adapters/actions/update-complement.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import { Complement } from "@/features/complement/domain/complement-schemas";
+import { complementUsecases } from "../../usecases/complement-usecases";
+
+export default async function updateComplement(options: Complement) {
+  return complementUsecases.updateCoursComplement({
+    coursComplement: options,
+  });
+}
