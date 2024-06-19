2609ae5d72c25f1689047c61b93e311440ef3518
Jason Suarez <swarecito@gmail.com>
2024-06-19 21:50:11 +0200

    refactor: Add AfterMenuBar component to FloatingEditor
    This commit adds the AfterMenuBar component to the FloatingEditor, allowing for additional content to be rendered after the floating menu and editor content. This enhancement provides more flexibility in customizing the FloatingEditor component and improves its usability in the application.

--- src/core/components/common/editor/FloatingEditor.tsx
+++ src/core/components/common/editor/FloatingEditor.tsx
@@ -4,11 +4,13 @@ import { EditorContent, FloatingMenu, useEditor } from "@tiptap/react";
 import { EXTENSIONS } from "@/core/components/constants/editor-extenstions";
 import { DebouncedFunc } from "lodash";
 import FloatingMenuBar from "@/core/components/common/editor/FloatingMenuBar";
+import AfterMenuBar from "./AfterMunuBar";
 
 function FloatingEditor(props: {
   content: string;
   debounceUpdateFn: DebouncedFunc<(content: string) => void>;
-  afterMenuBar?: React.ReactNode;
+  afterMenuBar?: boolean;
+  children?: React.ReactNode;
 }) {
   const editor = useEditor({
     extensions: EXTENSIONS,
@@ -27,7 +29,13 @@ function FloatingEditor(props: {
             <FloatingMenuBar editor={editor} />
           </FloatingMenu>
           <EditorContent editor={editor} />
-          {props.afterMenuBar}
+          {props.afterMenuBar && (
+            <div className="flex flex-col gap-4 ">
+              <AfterMenuBar
+                editor={editor}
+              >{props.children}</AfterMenuBar>
+            </div>
+          )}
         </>
       )}
     </>
