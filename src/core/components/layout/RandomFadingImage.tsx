06842c81ad2c6e68bc612ae9da6c27dc39178543
Jason Suarez <swarecito@gmail.com>
2024-06-22 00:58:15 +0200

    refactor: Add RandomFadingImage component for dynamic image effects
    This commit adds the RandomFadingImage component, which provides dynamic image effects such as random opacity, brightness, contrast, and glow. The component uses React hooks to manage state and intervals to update the effects at regular intervals. This enhancement improves the visual presentation and user experience of the application.

---
+++ src/core/components/layout/RandomFadingImage.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+const RandomFadingImage = ({ src, alt }: { src: string; alt: string }) => {
+  const [opacity, setOpacity] = useState(1);
+  const [filter, setFilter] = useState("none");
+
+  const [glow, setGlow] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomGlow = Math.random() * 10;
+      setGlow(randomGlow);
+    }, 2000); // changes glow every second
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomBrightness = Math.random() * 0.5 + 0.75; // Brightness between 0.75 and 1.25
+      const randomContrast = Math.random() * 50 + 100; // Contrast between 100% and 150%
+      setFilter(`brightness(${randomBrightness}) contrast(${randomContrast}%)`);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Generate a random opacity between 0.5 and 1
+      const randomOpacity = Math.random() * 0.7;
+      setOpacity(randomOpacity);
+    }, 2000); // changes opacity every second
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className="mx-auto hero-image absolute opacity-70 aspect-video overflow-hidden rounded-xl object-cover w-full lg:w-[70%]  "
+      style={{
+        opacity: opacity,
+        transition: "opacity 1s ease-in-out",
+        filter: filter,
+        boxShadow: `0 0 10px 5px rgba(255, 255, 255, ${glow})`,
+      }}
+    />
+  );
+};
+
+export default RandomFadingImage;
