4117d9c3ed14e6917d363fd3d1a2d00575c9ec61
Jason Suarez <swarecito@gmail.com>
2024-06-22 00:25:29 +0200

    refactor: Remove unused layout components and update RootLayout in cours module
    This commit removes the unused layout components in the cours module and updates the RootLayout component. The unused layout components, including Sidebar and Header, are deleted. The RootLayout component is modified to simplify the structure and improve performance by removing unnecessary elements and reducing nesting. These changes optimize the codebase and enhance the efficiency of the application.

--- src/app/cours/notes/[slug]/page.tsx
+++ src/app/cours/notes/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import LoadingSkeleton from "@/core/components/common/LoadingSkeleton";
 import NotesServerLayer from "@/app/profile/notes/[slug]/NotesServerLayer";
+import LayoutWithProps from "@/core/components/layout/LayoutWithProps";
 
 async function Page({
   params,
@@ -11,7 +12,9 @@ async function Page({
 }) {
   return (
     <Suspense fallback={<LoadingSkeleton />}>
-      <NotesServerLayer slug={params.slug} type="cours" />
+      <LayoutWithProps>
+        <NotesServerLayer slug={params.slug} type="cours" />
+      </LayoutWithProps>
     </Suspense>
   );
 }
