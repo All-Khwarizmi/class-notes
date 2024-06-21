b16a4259bb9e09b0df9c34a3bd79b6b9289187e2
Jason Suarez <swarecito@gmail.com>
2024-06-22 00:15:23 +0200

    refactor: Improve useDeleteComplement service error handling
    This commit refactors the useDeleteComplement service to improve its error handling. It adds logic to display an error toast message if the deleteComplement action returns an error. Additionally, it displays a success toast message and redirects the user to the specified path if the deletion is successful. These changes enhance the functionality and user experience of the application.

--- src/features/complement/application/adapters/services/useDeleteComplement.ts
+++ src/features/complement/application/adapters/services/useDeleteComplement.ts
@@ -1,11 +1,28 @@
 import { useMutation } from "@tanstack/react-query";
 import deleteComplement from "../actions/delete-complement";
+import { isLeft } from "fp-ts/lib/Either";
+import { toast } from "sonner";
 
 export default function useDeleteComplement() {
   return useMutation({
     mutationKey: ["delete-complement"],
     mutationFn: async (options: { id: string; redirectPath: string }) => {
-      return deleteComplement({ id: options.id });
+      const operationResult = await deleteComplement({ id: options.id });
+      if (isLeft(operationResult)) {
+        toast.error(
+          "An error occured while deleting the complement. Please try again later.",
+          { duration: 3000, position: "top-center" }
+        );
+        return;
+      } else {
+        toast.success("Complement deleted successfully.", {
+          duration: 3000,
+          position: "top-center",
+        });
+        window.location.href
+          ? window.location
+          : (window.location.href = options.redirectPath);
+      }
     },
   });
 }
