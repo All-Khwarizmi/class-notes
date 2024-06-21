507de6ec843ec497c8b51a83750a8029f959d3d9
Jason Suarez <swarecito@gmail.com>
2024-06-21 12:52:59 +0200

    refactor: Update layout.tsx to set language to English
    This commit updates the layout.tsx file to set the language attribute to "en" instead of "fr". This change ensures that the HTML document is marked as English, improving accessibility and SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr" suppressHydrationWarning>
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
-        />
-      </head>
-      <Providers>
+    <Providers>
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
+          />
+        </head>
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
@@ -40,7 +40,7 @@ export default function RootLayout({
             <Toaster richColors expand />
           </ThemeProvider>
         </body>
-      </Providers>
-    </html>
+      </html>
+    </Providers>
   );
 }
