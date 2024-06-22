4feb215d130cad80b83ee0512e24880d8e4931ac
Jason Suarez <swarecito@gmail.com>
2024-06-22 23:47:24 +0200

    feat: Add error handling and toast notifications to useAssignEvaluation

--- src/features/evaluation/application/adapters/services/useAssignEvaluation.ts
+++ src/features/evaluation/application/adapters/services/useAssignEvaluation.ts
@@ -1,12 +1,21 @@
 import { AssignEvaluationOptions } from "@/features/evaluation/domain/entities/evaluation-types";
 import { useMutation } from "@tanstack/react-query";
 import assignEvaluation from "../actions/assign-evaluation";
+import { isLeft } from "fp-ts/lib/Either";
+import { toast } from "sonner";
 
 export default function useAssignEvaluation() {
   return useMutation({
     mutationKey: ["assignEvaluation"],
     mutationFn: async (options: AssignEvaluationOptions) => {
-        return assignEvaluation(options);
+      const operationResult = await assignEvaluation(options);
+      if (isLeft(operationResult)) {
+        toast.error("An error occurred while assigning the evaluation", {
+          duration: 3000,
+        });
+        return;
+      }
+      toast.success("Evaluation assigned successfully", { duration: 3000 });
     },
   });
 }
