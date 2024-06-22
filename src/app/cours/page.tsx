26371ac33e3d6ae26995bed37b8595a2a34dfb4a
Jason Suarez <swarecito@gmail.com>
2024-06-22 14:41:22 +0200

    refactor: Update cours/page.tsx to use lazy loading for images

--- src/app/cours/page.tsx
+++ src/app/cours/page.tsx
@@ -1,5 +1,11 @@
-import AllCoursView from "@/features/cours-sequence/presentation/views/AllCoursView";
+import LoadingSkeleton from "@/core/components/common/LoadingSkeleton";
+import { Suspense } from "react";
+import CoursesServerLayer from "./CoursesServerLayer";
 
-export default async function Page() {
-  return <AllCoursView />;
+export default async function Page({ params }: { params: { slug: string } }) {
+  return (
+    <Suspense fallback={<LoadingSkeleton />}>
+      <CoursesServerLayer />
+    </Suspense>
+  );
 }
