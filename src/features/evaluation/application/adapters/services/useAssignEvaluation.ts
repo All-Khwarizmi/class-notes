34a11fee0447da0e1bcb0c4c0608e19fcb855874
Jason Suarez <swarecito@gmail.com>
2024-06-23 12:38:39 +0200

    refactor: Update AssignEvaluation component UI and functionality

--- src/features/evaluation/application/adapters/services/useAssignEvaluation.ts
+++ src/features/evaluation/application/adapters/services/useAssignEvaluation.ts
@@ -3,19 +3,24 @@ import { useMutation } from "@tanstack/react-query";
 import assignEvaluation from "../actions/assign-evaluation";
 import { isLeft } from "fp-ts/lib/Either";
 import { toast } from "sonner";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function useAssignEvaluation() {
+  const { fetchQuery } = useQueryClient();
   return useMutation({
     mutationKey: ["assignEvaluation"],
     mutationFn: async (options: AssignEvaluationOptions) => {
       const operationResult = await assignEvaluation(options);
       if (isLeft(operationResult)) {
-        toast.error("An error occurred while assigning the evaluation", {
+        toast.error("An error occurred while assiging the evaluation", {
           duration: 3000,
         });
         return;
       }
       toast.success("Evaluation assigned successfully", { duration: 3000 });
+    },
+    onSuccess: () => {
+      window.location.reload();
     },
   });
 }
