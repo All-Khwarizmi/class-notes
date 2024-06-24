dcdc8442ed4d648e632f05036c80fee31524abbc
Jason Suarez <swarecito@gmail.com>
2024-06-24 22:40:12 +0200

    feat: Add UPDATE_STUDENT query key to query-keys.ts
    This commit adds the UPDATE_STUDENT query key to the QUERY_KEYS object in the query-keys.ts file. The UPDATE_STUDENT query key is used to identify the query for updating a student in the student repository. This addition enhances the codebase by providing a standardized key for updating student data and improves code organization and maintainability.

--- src/core/query/ query-keys.ts
+++ src/core/query/ query-keys.ts
@@ -1,5 +1,6 @@
 export const QUERY_KEYS = {
   STUDENT: {
     DELETE: (options: string[] = []) => ["DELETE_STUDENT", ...options],
+    UPDATE: (options: string[] = []) => ["UPDATE_STUDENT", ...options],
   },
 };
