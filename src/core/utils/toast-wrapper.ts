5bd6a6ac9c08bd35486ebb193fde68383582f534
Jason Suarez <swarecito@gmail.com>
2024-06-24 22:19:49 +0200

    feat: Add toastWrapper utility for displaying toast messages
    This commit adds a new utility called `toastWrapper` to the `toast-wrapper.ts` file. The `toastWrapper` object provides methods for displaying toast messages with different types (success, error, warning, info). Each method takes in a message and optional toaster props, and calls the corresponding toast function from the `sonner` library. This addition enhances the codebase by providing a centralized and reusable way to display toast messages throughout the application.

---
+++ src/core/utils/toast-wrapper.ts
@@ -0,0 +1,59 @@
+import { ToasterProps, toast } from "sonner";
+
+export const TOAST_SHORT_DURATION = 2000;
+export const TOAST_LONG_DURATION = 5000;
+export const TOAST_POSITION = "top-right";
+
+/**
+ * A wrapper object for displaying toast messages.
+ */
+export const toastWrapper = {
+    /**
+     * Displays a success toast message.
+     * @param message - The message to be displayed.
+     * @param options - Optional toaster props.
+     */
+    success: (message: string, options?: ToasterProps) => {
+        toast.success(message, {
+            ...options,
+            duration: TOAST_SHORT_DURATION, // Default duration for success toast
+            position: TOAST_POSITION, // Default position for success toast
+        });
+    },
+    /**
+     * Displays an error toast message.
+     * @param message - The message to be displayed.
+     * @param options - Optional toaster props.
+     */
+    error: (message: string, options?: ToasterProps) => {
+        toast.error(message, {
+            ...options,
+            duration: TOAST_SHORT_DURATION, // Default duration for error toast
+            position: TOAST_POSITION, // Default position for error toast
+        });
+    },
+    /**
+     * Displays a warning toast message.
+     * @param message - The message to be displayed.
+     * @param options - Optional toaster props.
+     */
+    warning: (message: string, options?: ToasterProps) => {
+        toast.warning(message, {
+            ...options,
+            duration: TOAST_SHORT_DURATION, // Default duration for warning toast
+            position: TOAST_POSITION, // Default position for warning toast
+        });
+    },
+    /**
+     * Displays an info toast message.
+     * @param message - The message to be displayed.
+     * @param options - Optional toaster props.
+     */
+    info: (message: string, options?: ToasterProps) => {
+        toast.info(message, {
+            ...options,
+            duration: TOAST_SHORT_DURATION, // Default duration for info toast
+            position: TOAST_POSITION, // Default position for info toast
+        });
+    },
+};
