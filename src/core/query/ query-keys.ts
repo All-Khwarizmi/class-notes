ead0b8f913b4011de5abe647fd6d93901d63ebd7
Jason Suarez <swarecito@gmail.com>
2024-06-24 23:35:25 +0200

    feat: Add DELETE_CLASSE query key to query-keys.ts
    This commit adds the DELETE_CLASSE query key to the QUERY_KEYS object in the query-keys.ts file. The DELETE_CLASSE query key is used to identify the query for deleting a class in the class repository. This addition enhances the codebase by providing a standardized key for deleting class data and improves code organization and maintainability.

--- src/core/query/ query-keys.ts
+++ src/core/query/ query-keys.ts
@@ -3,4 +3,11 @@ export const QUERY_KEYS = {
     DELETE: (options: string[] = []) => ["DELETE_STUDENT", ...options],
     UPDATE: (options: string[] = []) => ["UPDATE_STUDENT", ...options],
   },
+  CLASSE: {
+    DELETE: (options: string[] = []) => ["DELETE_CLASSE", ...options],
+    // UPDATE: (options: string[] = []) => ["UPDATE_CLASSE", ...options],
+    // GET: (options: string[] = []) => ["GET_CLASSE", ...options],
+    // GET_ALL: (options: string[] = []) => ["GET_ALL_CLASSES", ...options],
+    // CREATE: (options: string[] = []) => ["CREATE_CLASSE", ...options],
+  },
 };
