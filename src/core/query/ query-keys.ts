9f6c5c9ac90de4a1e2405866334501f043ed574b
Jason Suarez <swarecito@gmail.com>
2024-06-28 12:00:50 +0200

    refactor: Enable CREATE functionality in QUERY_KEYS
    This commit enables the CREATE functionality in the QUERY_KEYS object in the query-keys.ts file. The CREATE property is uncommented, allowing users to create a new class by calling the "CREATE_CLASSE" action with the specified options. This change enhances the functionality of the application by providing the ability to create classes through the QUERY_KEYS object.

--- src/core/query/ query-keys.ts
+++ src/core/query/ query-keys.ts
@@ -8,6 +8,6 @@ export const QUERY_KEYS = {
     // UPDATE: (options: string[] = []) => ["UPDATE_CLASSE", ...options],
     // GET: (options: string[] = []) => ["GET_CLASSE", ...options],
     // GET_ALL: (options: string[] = []) => ["GET_ALL_CLASSES", ...options],
-    // CREATE: (options: string[] = []) => ["CREATE_CLASSE", ...options],
+    CREATE: (options: string[] = []) => ["CREATE_CLASSE", ...options],
   },
 };
