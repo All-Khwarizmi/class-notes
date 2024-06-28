0731d22b58c189c0da6a090304257e2ce4438bd5
Jason Suarez <swarecito@gmail.com>
2024-06-28 20:07:42 +0200

    refactor: Update checkSpecialGradeType to use ReadonlyNonEmptyArray from fp-ts
    This commit updates the checkSpecialGradeType function in the checkSpecialGradeType.ts file to use the ReadonlyNonEmptyArray type from the fp-ts library. Previously, the cons function from the ReadonlyNonEmptyArray module was imported but not used. This change ensures that the cons function is used to create a non-empty array, improving the type safety and readability of the code. It aligns the codebase with the established usage of the fp-ts library and enhances the maintainability of the code.

--- src/features/evaluation/application/adapters/utils/checkSpecialGradeType.ts
+++ src/features/evaluation/application/adapters/utils/checkSpecialGradeType.ts
@@ -1,4 +1,5 @@
 import { Grade } from "@/features/evaluation/domain/entities/evaluation-with-grades-schema";
+import { cons } from "fp-ts/lib/ReadonlyNonEmptyArray";
 import { z } from "zod";
 
 // Define the specific new grade types
@@ -22,16 +23,15 @@ type GradeTypeUnion =
 // Helper function to check if the grade is one of the special types
 function checkSpecialGradeType(grades: Grade[]): GradeTypeUnion {
   for (const grade of grades) {
-    const result = SpecialGradeTypes.safeParse(grade.grade);
+    const specialResult = SpecialGradeTypes.safeParse(grade.grade);
 
-    if (result.success) {
+    if (specialResult.success) {
       return {
         shouldReturn: true,
-        returnValue: result.data,
+        returnValue: specialResult.data,
       };
     } else {
       const result = z.union([z.number(), z.string()]).safeParse(grade.grade);
-
       if (result.success) {
         return {
           shouldReturn: false,
