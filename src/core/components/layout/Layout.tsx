da4b0f8f132082e036d0c3c1f02cc160d03af69d
Jason Suarez <swarecito@gmail.com>
2024-06-29 12:22:30 +0200

    refactor: Update Layout component to use Layout.Children for rendering children components

--- src/core/components/layout/Layout.tsx
+++ src/core/components/layout/Layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { LayoutWithPropsProps } from "./LayoutWithProps";
 import LayoutContext, { useLayoutContext } from "./LayoutContext";
@@ -10,6 +12,19 @@ import ErrorDialog from "../common/ErrorDialog";
 import { Home } from "lucide-react";
 import LoadingSkeleton from "../common/LoadingSkeleton";
 
+/**
+ * Renders the layout component with the provided props.
+ *
+ * @param {LayoutWithPropsProps} props - The props for the layout component.
+ * @param {React.ReactNode} props.children - The content to be rendered inside the layout.
+ * @param {NavItem[]} props.navItems - The navigation items to be displayed in the header and sidebar.
+ * @param {boolean} [props.isEmpty=false] - Indicates whether the layout is empty.
+ * @param {boolean} [props.notFound=false] - Indicates whether the layout represents a not found page.
+ * @param {boolean} [props.nothingToShow=false] - Indicates whether there is nothing to show in the layout.
+ * @param {boolean} props.isError - Indicates whether an error occurred.
+ * @param {boolean} props.isLoading - Indicates whether the layout is in a loading state.
+ * @returns {React.ReactNode} The rendered layout component.
+ */
 function Layout({
   children,
   navItems,
@@ -35,15 +50,13 @@ function Layout({
         isLoading,
       }}
     >
-      <>
-        <Header navItems={isEmpty === true ? [defaultNavItem] : navItems} />
-        <section className="flex h-full w-full border-collapse overflow-hidden">
-          <Sidebar navItems={isEmpty === true ? [defaultNavItem] : navItems} />
-          <section className="h-full flex-1  pt-4 px-4 overflow-x-hidden">
-            {children}
-          </section>
+      <Header navItems={isEmpty === true ? [defaultNavItem] : navItems} />
+      <section className="flex h-full w-full border-collapse overflow-hidden">
+        <Sidebar navItems={isEmpty === true ? [defaultNavItem] : navItems} />
+        <section className="h-full flex-1  pt-4 px-4 overflow-x-hidden">
+          {children}
         </section>
-      </>
+      </section>
     </LayoutContext.Provider>
   );
 }
@@ -78,7 +91,7 @@ Layout.LoadingSkeleton = function LayoutLoadingSkeleton() {
 
 Layout.Children = function LayoutChildren() {
   const { children } = useLayoutContext();
-  return <>{children}</>;
+  return children;
 };
 
 export default Layout;
