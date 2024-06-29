bde01a37416d7ef6aec711ef41d03fbcbb779173
Jason Suarez <swarecito@gmail.com>
2024-06-29 16:42:10 +0200

    refactor: Add "/login" to publicRoutes in authMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ https://nextjs.org/docs/app/building-your-application/authentication
 */
 
 export default authMiddleware({
-  publicRoutes: ["/", "/contact", "/about", "/sign-in", "/sign-up", "/spaces"],
+  publicRoutes: ["/", "/contact", "/about", "/sign-in", "/sign-up", "/spaces", "/login"],
 });
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
