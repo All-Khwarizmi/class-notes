77ffcade35c229c0557a097f97a34df7cca0fe81
Jason Suarez <swarecito@gmail.com>
2024-06-29 10:56:15 +0200

    refactor: Update QUERY_KEYS to include GET_ALL option
    This commit updates the QUERY_KEYS object in the query-keys.ts file to include the GET_ALL option. Previously, this option was commented out, but it has been uncommented to enable retrieving all classes. This change enhances the functionality of the application by allowing users to fetch all classes instead of individual ones. It aligns with the recent user commits that involve class creation and reloading the page after class creation. Additionally, it follows the established convention of using the "refactor" prefix for code changes that improve or optimize existing functionality.

--- src/core/query/ query-keys.ts
+++ src/core/query/ query-keys.ts
@@ -9,7 +9,7 @@ export const QUERY_KEYS = {
     DELETE: (options: string[] = []) => ["DELETE_CLASSE", ...options],
     // UPDATE: (options: string[] = []) => ["UPDATE_CLASSE", ...options],
     // GET: (options: string[] = []) => ["GET_CLASSE", ...options],
-    // GET_ALL: (options: string[] = []) => ["GET_ALL_CLASSES", ...options],
+    GET_ALL: (options: string[] = []) => ["GET_ALL_CLASSES", ...options],
     CREATE: (options: string[] = []) => ["CREATE_CLASSE", ...options],
   },
 };
