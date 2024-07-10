fc7ed86bdb01465c6398cb79c83fd4841ced6e37
Jason Suarez <swarecito@gmail.com>
2024-07-10 13:31:04 +0200

    refactor: Add BASE_GET_ALL query key for evaluations in query-keys.ts
    This commit adds the BASE_GET_ALL query key to the EVALUATIONS object in query-keys.ts. This query key is used to fetch all evaluations base data. The CREATE and DELETE query keys are commented out for now. This change improves the organization of query keys and follows the established convention of using the "refactor" prefix for code changes that improve the code structure or organization.

--- src/features/evaluation/application/adapters/services/useGetEvaluationsBaseList.ts
+++ src/features/evaluation/application/adapters/services/useGetEvaluationsBaseList.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import getEvaluations from "../actions/get-evaluations";
+import { QUERY_KEYS } from "@/core/query/ query-keys";
 
 export default function useGetEvaluationsBaseList(options: { userId: string }) {
   return useQuery({
-    queryKey: ["getEvaluationsBaseList"],
+    queryKey: QUERY_KEYS.EVALUATIONS.BASE_GET_ALL(),
     queryFn: async () => {
       return await getEvaluations(options);
     },
