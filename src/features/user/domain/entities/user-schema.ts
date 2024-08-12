91b1569b0fa38ef9e6d4ec59dfa409aff066aa1d
Jason Suarez <swarecito@gmail.com>
2024-08-12 11:42:39 +0200

    feat: Update userSchema with default schoolSubject and country
    This commit updates the userSchema in the user-domain/entities/user-schema.ts file. The default value for the schoolSubject field is set to "Arts", and the country field is added with an enum of countries including USA, France, Spain, Germany, Australia, and England. This change enhances the user schema by providing a default value for schoolSubject and adding support for country selection. It follows the established convention of using the "feat" prefix for code changes that introduce new features or functionality.

--- src/features/user/domain/entities/user-schema.ts
+++ src/features/user/domain/entities/user-schema.ts
@@ -1,8 +1,21 @@
 import { z } from "zod";
+import { GlobalEducationSubjectsSchema } from "./education-systems/global";
+// Définition de l'enum pour les pays
+const CountrySchema = z.enum([
+  "USA",
+  "France",
+  "Spain",
+  "Germany",
+  "Australia",
+  "England",
+]);
+
+export type CountryType = z.infer<typeof CountrySchema>;
 
 export const userSchema = z.object({
   _id: z.string(),
-  schoolSubject: z.string().optional(),
+  schoolSubject: GlobalEducationSubjectsSchema.default("Arts"),
+  country: CountrySchema,
   name: z.string().optional(),
   onboarding: z.boolean().optional(),
   image: z.string().optional(),
