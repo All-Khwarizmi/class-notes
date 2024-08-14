34821968a125526acf9c149551d5d749cc85deec
Jason Suarez <swarecito@gmail.com>
2024-08-14 21:57:20 +0200

    refactor: Update DashboardServerLayer to use react-query for prefetching

--- src/app/dashboard/DashboardServerLayer.tsx
+++ src/app/dashboard/DashboardServerLayer.tsx
@@ -1,5 +1,12 @@
+import { QUERY_KEYS } from "@/core/query/ query-keys";
 import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
+import { classeUsecases } from "@/features/classe/application";
 import DashboardGrid from "@/features/dashboard/presentation/views/DashboardGrid";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import { isLeft } from "fp-ts/lib/Either";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -9,7 +16,24 @@ async function DashboardServerLayer() {
   if (isLeft(authUser)) {
     redirect("/login");
   }
-  return <DashboardGrid userId={authUser.right.userId} />;
+  const queryClient = new QueryClient();
+
+  const queryBulk = [
+    queryClient.prefetchQuery({
+      queryKey: [QUERY_KEYS.VISIBILITY.GET_ALL()],
+      queryFn: async () => {
+        return classeUsecases.getVisibility({ userId: authUser.right.userId });
+      },
+    }),
+  ];
+
+  await Promise.all(queryBulk);
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DashboardGrid userId={authUser.right.userId} />
+    </HydrationBoundary>
+  );
 }
 
 export default DashboardServerLayer;
