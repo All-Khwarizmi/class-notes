65c5433965dda80650cbe2b2cb59dcfe57dab8ea
Jason Suarez <swarecito@gmail.com>
2024-08-14 08:02:41 +0200

    fix: Update QuickLinksCard component link to "/classes/add"

--- src/features/dashboard/presentation/components/QuickLinksCard.tsx
+++ src/features/dashboard/presentation/components/QuickLinksCard.tsx
@@ -28,12 +28,12 @@ function QuickLinksCard() {
           Create Sequence
         </Link>
         <Link
-          href="#"
+          href="classes/add"
           className="inline-flex items-center justify-between gap-2 rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
           prefetch={false}
         >
           <div className="h-4 w-4" />
-          Create Class
+          Create Classe
         </Link>
         <Link
           href="#"
