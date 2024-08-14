0bb9fd76e5d7b13eb03d020396892fef1356ff70
Jason Suarez <swarecito@gmail.com>
2024-08-14 22:15:25 +0200

    refactor: Update useVisibilityLogic to toggle visibility state and update visibility options
    This commit refactors the useVisibilityLogic hook in the useVisibilityLogic.ts file. It adds a new function, toggleStateVisibility, which allows toggling the visibility state for different types of items (classe, sequence, cours, complement) based on the provided arguments. It also updates the visibility state and triggers the updateVisibility mutation to update the visibility options in the database. This change enhances the code by improving the flexibility and functionality of the useVisibilityLogic hook.

--- src/features/dashboard/application/adapters/services/useVisibilityLogic.ts
+++ src/features/dashboard/application/adapters/services/useVisibilityLogic.ts
@@ -1,7 +1,9 @@
 import useGetVisibility from "@/features/classe/application/adapters/services/useGetVisibility";
+import useUpdateVisibility from "@/features/classe/application/adapters/services/useUpdateVisibility";
 import {
   FlatVisibilityType,
   flatVisibilityType,
+  toggleVisibility,
 } from "@/features/classe/domain/visibility-schema";
 import { isRight } from "fp-ts/lib/Either";
 import { useState, useEffect } from "react";
@@ -13,16 +15,43 @@ export function useVisibilityLogic(options: { userId: string }) {
     isLoading,
     isError,
     error,
+    refetch,
   } = useGetVisibility({ userId: options.userId });
+  const { mutate: updateVisibility } = useUpdateVisibility();
   useEffect(() => {
     if (visibility && !isLoading && !isError && isRight(visibility)) {
       setVisibilityState(flatVisibilityType(visibility.right));
     }
   }, [visibility]);
-
+  const toggleStateVisibility = (args: {
+    type: "classe" | "sequence" | "cours" | "complement";
+    typeId: string;
+    publish: boolean;
+  }) => {
+    setVisibilityState(
+      toggleVisibility(visibilityState!, {
+        type: args.type,
+        typeId: args.typeId,
+        publish: args.publish,
+      })
+    );
+    updateVisibility(
+      {
+        userId: options.userId,
+        type: args.type,
+        typeId: args.typeId,
+        publish: args.publish,
+      },
+      {
+        onSuccess: () => {
+          refetch();
+        },
+      }
+    );
+  };
   return {
     visibilityState,
-    setVisibilityState,
+    toggleStateVisibility,
     isLoading,
     isError,
     error,
