0b80a2b59bdb70cb6fb94e6b9ed33b11167112c7
Jason Suarez <swarecito@gmail.com>
2024-08-14 22:56:23 +0200

    refactor: Update DashboardGrid component to use VisibilityManagementComponent
    This commit refactors the DashboardGrid component in the DashboardGrid.tsx file. It replaces the VisibilityManagementCard component with the VisibilityManagementComponent component. This change improves code consistency and removes the unused import of VisibilityManagementCard.

--- src/features/dashboard/presentation/views/DashboardGrid.tsx
+++ src/features/dashboard/presentation/views/DashboardGrid.tsx
@@ -1,26 +1,16 @@
 "use client";
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/YOdg8xf5YDC
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
-
-
-import SequencesCard from "../components/SequencesCard";
 import QuickLinksCard from "../components/QuickLinksCard";
 import RessourceLinksCard from "../components/RessourceLinksCard";
-import VisibilityManagementCard from "../components/VisibilityMangementCard";
+import VisibilityManagementComponent from "../components/VisibilityManagementPanel";
 
 export default function DashboardGrid(props: { userId: string }) {
   return (
     <div className="flex flex-col w-full min-h-screen ">
       <main className="flex-1 grid gap-4 p-4 md:p-6 lg:grid-cols-3 xl:grid-cols-4">
-        <SequencesCard />
+        {/* <SequencesCard /> */}
+        <VisibilityManagementComponent userId={props.userId} />
         <QuickLinksCard />
-        <RessourceLinksCard
-          userId={props.userId}
-        />
-        <VisibilityManagementCard />
+        <RessourceLinksCard userId={props.userId} />
       </main>
     </div>
   );
