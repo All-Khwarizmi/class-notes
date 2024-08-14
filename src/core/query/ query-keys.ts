f4c4c42f1d703df8050bf069eeb69a5ddeb10087
Jason Suarez <swarecito@gmail.com>
2024-08-14 19:14:34 +0200

    refactor: Add GET_ALL_STUDENTS and GET_ALL_CLASSE_SEQUENCES query keys
    This commit adds the GET_ALL_STUDENTS and GET_ALL_CLASSE_SEQUENCES query keys to the query-keys.ts file. These query keys are used to fetch all students and all classe sequences respectively. This change enhances the functionality of the code by providing convenient access to these data sets.

--- src/core/query/ query-keys.ts
+++ src/core/query/ query-keys.ts
@@ -4,6 +4,7 @@ export const QUERY_KEYS = {
     UPDATE: (options: string[] = []) => ["UPDATE_STUDENT", ...options],
     ADD: (options: string[] = []) => ["ADD_STUDENT", ...options],
     ADD_MANY: (options: string[] = []) => ["ADD_MANY_STUDENTS", ...options],
+    GET_ALL: (options: string[] = []) => ["GET_ALL_STUDENTS", ...options],
   },
   CLASSE: {
     DELETE: (options: string[] = []) => ["DELETE_CLASSE", ...options],
@@ -11,6 +12,10 @@ export const QUERY_KEYS = {
     // GET: (options: string[] = []) => ["GET_CLASSE", ...options],
     GET_ALL: (options: string[] = []) => ["GET_ALL_CLASSES", ...options],
     CREATE: (options: string[] = []) => ["CREATE_CLASSE", ...options],
+    CLASSE_SEQUENCES_GET_ALL: (options: string[] = []) => [
+      "GET_ALL_CLASSE_SEQUENCES",
+      ...options,
+    ],
   },
   SEQUENCE: {
     DELETE: (options: string[] = []) => ["DELETE_SEQUENCE", ...options],
@@ -20,6 +25,10 @@ export const QUERY_KEYS = {
   EVALUATIONS: {
     BASE_GET_ALL: (options: string[] = []) => [
       "GET_ALL_EVALUATIONS_BASE",
+      ...options,
+    ],
+    COMPOUND_GET_ALL: (options: string[] = []) => [
+      "GET_ALL_EVALUATIONS_COMPOUND",
       ...options,
     ],
     // CREATE: (options: string[] = []) => ["CREATE_EVALUATION", ...options],
