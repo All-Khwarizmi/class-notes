4124c4bb2498d379c84b70595cd0d520b9a3d5c5
Jason Suarez <swarecito@gmail.com>
2024-08-15 12:10:56 +0200

    refactor: Update toastWrapper with new dismiss and loading functions
    This commit updates the toastWrapper object in the toast-wrapper.ts file. It adds two new functions, dismiss and loading, to handle dismissing toast messages and displaying loading toast messages respectively. These changes enhance the functionality of the toastWrapper and provide more options for displaying toast messages in the application.

--- src/core/utils/toast-wrapper.ts
+++ src/core/utils/toast-wrapper.ts
@@ -8,52 +8,62 @@ export const TOAST_POSITION = "top-right";
  * A wrapper object for displaying toast messages.
  */
 export const toastWrapper = {
-    /**
-     * Displays a success toast message.
-     * @param message - The message to be displayed.
-     * @param options - Optional toaster props.
-     */
-    success: (message: string, options?: ToasterProps) => {
-        toast.success(message, {
-            ...options,
-            duration: TOAST_SHORT_DURATION, // Default duration for success toast
-            position: TOAST_POSITION, // Default position for success toast
-        });
-    },
-    /**
-     * Displays an error toast message.
-     * @param message - The message to be displayed.
-     * @param options - Optional toaster props.
-     */
-    error: (message: string, options?: ToasterProps) => {
-        toast.error(message, {
-            ...options,
-            duration: TOAST_SHORT_DURATION, // Default duration for error toast
-            position: TOAST_POSITION, // Default position for error toast
-        });
-    },
-    /**
-     * Displays a warning toast message.
-     * @param message - The message to be displayed.
-     * @param options - Optional toaster props.
-     */
-    warning: (message: string, options?: ToasterProps) => {
-        toast.warning(message, {
-            ...options,
-            duration: TOAST_SHORT_DURATION, // Default duration for warning toast
-            position: TOAST_POSITION, // Default position for warning toast
-        });
-    },
-    /**
-     * Displays an info toast message.
-     * @param message - The message to be displayed.
-     * @param options - Optional toaster props.
-     */
-    info: (message: string, options?: ToasterProps) => {
-        toast.info(message, {
-            ...options,
-            duration: TOAST_SHORT_DURATION, // Default duration for info toast
-            position: TOAST_POSITION, // Default position for info toast
-        });
-    },
+  dismiss: (id?: number | string) => {
+    toast.dismiss(id);
+  },
+  loading: (message?: string, options?: ToasterProps) => {
+    return toast.loading(message, {
+      ...options,
+      duration: TOAST_LONG_DURATION, // Default duration for loading toast
+      position: TOAST_POSITION, // Default position for loading toast
+    });
+  },
+  /**
+   * Displays a success toast message.
+   * @param message - The message to be displayed.
+   * @param options - Optional toaster props.
+   */
+  success: (message: string, options?: ToasterProps) => {
+    toast.success(message, {
+      ...options,
+      duration: TOAST_SHORT_DURATION, // Default duration for success toast
+      position: TOAST_POSITION, // Default position for success toast
+    });
+  },
+  /**
+   * Displays an error toast message.
+   * @param message - The message to be displayed.
+   * @param options - Optional toaster props.
+   */
+  error: (message: string, options?: ToasterProps) => {
+    toast.error(message, {
+      ...options,
+      duration: TOAST_SHORT_DURATION, // Default duration for error toast
+      position: TOAST_POSITION, // Default position for error toast
+    });
+  },
+  /**
+   * Displays a warning toast message.
+   * @param message - The message to be displayed.
+   * @param options - Optional toaster props.
+   */
+  warning: (message: string, options?: ToasterProps) => {
+    toast.warning(message, {
+      ...options,
+      duration: TOAST_SHORT_DURATION, // Default duration for warning toast
+      position: TOAST_POSITION, // Default position for warning toast
+    });
+  },
+  /**
+   * Displays an info toast message.
+   * @param message - The message to be displayed.
+   * @param options - Optional toaster props.
+   */
+  info: (message: string, options?: ToasterProps) => {
+    toast.info(message, {
+      ...options,
+      duration: TOAST_SHORT_DURATION, // Default duration for info toast
+      position: TOAST_POSITION, // Default position for info toast
+    });
+  },
 };
