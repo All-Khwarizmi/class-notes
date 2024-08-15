ec0b84b811620a3c727596931012972bc4d9f738
Jason Suarez <swarecito@gmail.com>
2024-08-15 23:00:46 +0200

    refactor: Remove commented out code for SequencesCard in DashboardGrid.tsx

--- src/features/dashboard/presentation/views/DashboardGrid.tsx
+++ src/features/dashboard/presentation/views/DashboardGrid.tsx
@@ -7,7 +7,6 @@ export default function DashboardGrid(props: { userId: string }) {
   return (
     <div className="flex flex-col w-full min-h-screen ">
       <main className="flex-1 grid gap-4 p-4 md:p-6 lg:grid-cols-3 xl:grid-cols-4">
-        {/* <SequencesCard /> */}
         <VisibilityManagementComponent userId={props.userId} />
         <QuickLinksCard />
         <RessourceLinksCard userId={props.userId} />
