84030de33f1e90b6a9e6f6f4e335b950ece5bedc
Jason Suarez <swarecito@gmail.com>
2024-08-16 23:57:14 +0200

    refactor: Add Accordion component
    This commit adds the Accordion component to the project. The Accordion component is a UI component that allows for collapsible sections of content. It includes the following sub-components: AccordionItem, AccordionTrigger, and AccordionContent. This addition improves the user experience by providing a more organized and interactive way to display content.

--- src/features/comp-cat/presentation/views/CompetencesTable.tsx
+++ src/features/comp-cat/presentation/views/CompetencesTable.tsx
@@ -8,11 +8,30 @@ import {
   TableHeader,
   TableRow,
 } from "@/core/components/ui/table";
-
-import { Category, Competence } from "../../domain/entities/schemas";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/core/components/ui/accordion";
+import {
+  Category,
+  Competence,
+  groupCompetencesByCategory,
+} from "../../domain/entities/schemas";
 import { Button } from "@/core/components/ui/button";
 import Link from "next/link";
 import { Plus } from "lucide-react";
+import { useMemo } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/core/components/ui/card";
+import { TypographyH4 } from "@/core/components/common/Typography";
 
 export default function CompetencesTable({
   competences,
@@ -23,41 +42,74 @@ export default function CompetencesTable({
   categories: Category[];
   userId: string;
 }) {
+  const groupedCompetences = useMemo(
+    () => groupCompetencesByCategory(competences),
+    [competences]
+  );
   return (
-    <>
+    <main className="  rounded-md p-4 ">
+      <h1 className="text-3xl font-bold pb-8 ">
+        <span>Competences</span>
+      </h1>
       <Table>
-        <TableCaption>Competences</TableCaption>
+        <TableCaption>Add competences or category</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead>Category</TableHead>
-
-            <TableHead>Competence</TableHead>
-
-            <TableHead>Description</TableHead>
-
-            <TableHead>Actions</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {competences.map((competence) => {
-            return (
-              <TableRow key={competence._id}>
-                <TableCell>{competence.category}</TableCell>
-                <TableCell>{competence.name}</TableCell>
-                <TableCell>{competence.description}</TableCell>
+          {
+            // Loop through the groupedCompetences
+            groupedCompetences.map((group) => (
+              <TableRow key={group.category}>
+                <TableCell colSpan={4} className="font-bold">
+                  {/* Make it an accordion that when open displays an indented list of competences cards  */}
+                  <Accordion type="multiple">
+                    <AccordionItem value={group.category}>
+                      <AccordionTrigger>
+                        <TypographyH4 text={group.category} />
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        <div className="pl-4 space-y-4">
+                          {group.competences.map((competence) => (
+                            <Card
+                              key={competence._id}
+                              className="bg-background shadow-inner shadow-slate-900 "
+                            >
+                              <CardHeader>
+                                <CardTitle>{competence.name}</CardTitle>
+                              </CardHeader>
+                              <CardContent>
+                                <CardDescription>
+                                  {competence.description}
+                                </CardDescription>
+                              </CardContent>
+                              <CardFooter>
+                                <Link href={`/competences/${competence._id}`}>
+                                  <Button>Edit</Button>
+                                </Link>
+                              </CardFooter>
+                            </Card>
+                          ))}
+                        </div>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </TableCell>
               </TableRow>
-            );
-          })}
+            ))
+          }
         </TableBody>
       </Table>
       {/* Add Competence button */}
       <div className="flex justify-center py-4">
-        <Link  href={"/competences/add"}>
-          <Button >
+        <Link href={"/competences/add"}>
+          <Button>
             <Plus size={20} />
           </Button>
         </Link>
       </div>
-    </>
+    </main>
   );
 }
