54b1f553cd60cb77df06e60a038d43a7f4a1724b
Jason Suarez <swarecito@gmail.com>
2024-08-16 23:04:50 +0200

    refactor: Update CompetenceForm component layout
    This commit updates the CompetenceForm component in the CompetenceForm.tsx file to improve its layout and functionality. It includes the following changes:
    - Reorganizes the structure of the form elements for better readability and maintainability.
    - Adds a title to the form for clarity.
    - Adjusts the styling of the form to match the overall design of the application.
    - Updates the Select component to allow users to select a category for the competence.
    
    These changes enhance the user experience and make it easier to add new competences.

--- src/features/comp-cat/presentation/views/CompetenceForm.tsx
+++ src/features/comp-cat/presentation/views/CompetenceForm.tsx
@@ -61,83 +61,89 @@ export default function CompetenceForm({
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel htmlFor={field.name}>Name</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="React" />
-                </FormControl>
-                <FormDescription>
-                  Enter the name of the competence
-                </FormDescription>
-              </FormItem>
-            );
-          }}
-        />
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel htmlFor={field.name}>Description</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    placeholder="React is a JavaScript library for building user interfaces"
-                  />
-                </FormControl>
-                <FormDescription>
-                  Enter the description of the competence
-                </FormDescription>
-              </FormItem>
-            );
-          }}
-        />
-
-        <div className="flex gap-2">
-          <Select
-            value={category}
-            onValueChange={(value) => {
-              form.setValue("category", value);
-              setCategory(value);
+    <div className="w-full max-w-2xl mx-auto  shadow-slate-800 shadow-inner rounded-md p-4">
+      <Form {...form}>
+        <h1 className="text-3xl font-semibold py-8">Add new competence</h1>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 ">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel htmlFor={field.name}>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="React" />
+                  </FormControl>
+                </FormItem>
+              );
             }}
-          >
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select a category" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                {categories.map((category) => (
-                  <SelectItem key={category._id} value={category.name}>
-                    {category.name}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <CustomDialog open={open} setOpen={setOpen} title="Add new category">
-            <CategoryForm userId={userId} />
-          </CustomDialog>
-        </div>
-
-        <Button
-          variant={"outline"}
-          onClick={() => {
-            onSubmit(form.getValues());
-          }}
-          type="submit"
-          className="btn"
-        >
-          <Check className="text-green-500" size={20} />
-        </Button>
-      </form>
-    </Form>
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel htmlFor={field.name}>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="React is a JavaScript library for building user interfaces"
+                    />
+                  </FormControl>
+                </FormItem>
+              );
+            }}
+          />
+          <div className="flex justify-between pt-4">
+            <div className="space-y-1">
+              <div className="flex gap-2">
+                <Select
+                  value={category}
+                  onValueChange={(value) => {
+                    form.setValue("category", value);
+                    setCategory(value);
+                  }}
+                >
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select a category" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      {categories.map((category) => (
+                        <SelectItem key={category._id} value={category.name}>
+                          {category.name}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <CustomDialog
+                  open={open}
+                  setOpen={setOpen}
+                  title="Add new category"
+                >
+                  <CategoryForm userId={userId} />
+                </CustomDialog>
+              </div>
+              <FormDescription>
+                Select the category of the competence
+              </FormDescription>
+            </div>
+            <Button
+              variant={"outline"}
+              onClick={() => {
+                onSubmit(form.getValues());
+              }}
+              type="submit"
+              className="btn"
+            >
+              <Check className="text-green-500" size={20} />
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
   );
 }
