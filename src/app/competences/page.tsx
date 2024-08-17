45f860a705bed519d2be47ca5d5fb2f951770e14
Jason Suarez <swarecito@gmail.com>
2024-08-17 22:46:14 +0200

    refactor: Update CompetencesTable to prefetch comp cat

--- src/app/competences/page.tsx
+++ src/app/competences/page.tsx
@@ -1,9 +1,12 @@
-import ErrorDialog from "@/core/components/common/ErrorDialog";
 import { QUERY_KEYS } from "@/core/query/ query-keys";
 import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
 import { compCatUsecases } from "@/features/comp-cat/application/usecases/comp-cat-usecases";
 import CompetencesTable from "@/features/comp-cat/presentation/views/CompetencesTable";
-import { QueryClient } from "@tanstack/react-query";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import { isLeft } from "fp-ts/lib/Either";
 import { redirect } from "next/navigation";
 
@@ -14,7 +17,7 @@ export default async function Page() {
     redirect("/login");
   }
   await queryClient.prefetchQuery({
-    queryKey: [QUERY_KEYS.COMP_CAT.GET_ALL()],
+    queryKey: QUERY_KEYS.COMP_CAT.GET_ALL(),
     queryFn: () =>
       compCatUsecases.getCategoriesAndCompetences({
         userId: authUser.right.userId,
@@ -22,10 +25,8 @@ export default async function Page() {
   });
 
   return (
-    <>
-      <CompetencesTable
-
-      />
-    </>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <CompetencesTable userId={authUser.right.userId} />
+    </HydrationBoundary>
   );
 }
