1a19a0d98819a3feac0df985269d719c4b410810
Jason Suarez <swarecito@gmail.com>
2024-08-17 17:47:01 +0200

    refactor: Update CoursSequenceCard component to include delete option
    This commit updates the CoursSequenceCard component in the CoursSequenceCard.tsx file to include a delete option. The deleteOption prop is now passed to the component, and when it is true, a delete button is displayed. The deleteSequence prop is also passed to the component to handle the delete functionality.
    
    These changes enhance the functionality and flexibility of the CoursSequenceCard component.

--- src/features/cours-sequence/presentation/components/CoursSequenceCard.tsx
+++ src/features/cours-sequence/presentation/components/CoursSequenceCard.tsx
@@ -3,11 +3,16 @@
  * @see https://v0.dev/t/st7u8l4epdA
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
-import { CardContent, CardFooter, Card } from "@/core/components/ui/card";
-import { Eye, PenIcon } from "lucide-react";
-import AfterMenuButton from "@/core/components/common/editor/AfterMenuButton";
+import {
+  CardContent,
+  CardFooter,
+  Card,
+  CardHeader,
+} from "@/core/components/ui/card";
+import {  Settings, Trash } from "lucide-react";
 import { Button } from "@/core/components/ui/button";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export default function CoursSequenceCard({
   title,
@@ -18,7 +23,11 @@ export default function CoursSequenceCard({
   showViewButton,
   pathToView,
   spacesMode,
+  deleteOption,
+  deleteSequence,
 }: {
+  deleteOption?: boolean;
+  deleteSequence?: () => void;
   title: string;
   description: string;
   imageUrl: string;
@@ -30,13 +39,15 @@ export default function CoursSequenceCard({
 }) {
   return (
     <Card className=" md:max-w-sm">
-      <img
-        alt="Product Image"
-        className="rounded-t-lg object-cover w-full aspect-[3/2]"
-        height={400}
-        src={imageUrl}
-        width={600}
-      />
+      <CardHeader className="">
+        <img
+          alt="Product Image"
+          className="rounded-t-lg object-cover w-full aspect-[3/2]"
+          height={400}
+          src={imageUrl}
+          width={600}
+        />
+      </CardHeader>
       <CardContent className="p-4 space-y-2 ">
         <h3 className="text-xl font-bold">{title}</h3>
         <p className="text-gray-500 dark:text-gray-400">{description}</p>
@@ -53,19 +64,24 @@ export default function CoursSequenceCard({
       </CardContent>
       <CardFooter className="p-4">
         <div className="flex gap-2 w-full">
-          {!spacesMode && (
-            <AfterMenuButton
-              addButton={true}
-              path={path}
-              icon={<PenIcon size={12} />}
-            />
-          )}
           {showViewButton && (
             <div className="flex justify-center w-full h-full">
               <Button variant="outline" className="">
                 <Link href={pathToView ?? "/sequences"}>View Sequence</Link>
               </Button>
             </div>
+          )}
+          {!spacesMode && (
+            <Link href={path} className=" ">
+              <Settings />
+            </Link>
+          )}
+          {deleteOption && deleteSequence && (
+            <Trash
+              size={28}
+              onClick={deleteSequence}
+              className={cn(" text-red-500 cursor-pointer ")}
+            />
           )}
         </div>
       </CardFooter>
