c33c6e8bba3b3519bfffc30d59102a1dd9a5d595
Jason Suarez <swarecito@gmail.com>
2024-08-19 20:34:52 +0200

    refactor: prefetch competences in server layer

--- src/app/evaluations/add/AddEvaluationBaseServerLayer.tsx
+++ src/app/evaluations/add/AddEvaluationBaseServerLayer.tsx
@@ -1,5 +1,8 @@
+import { QUERY_KEYS } from "@/core/query/ query-keys";
 import { authUseCases } from "@/features/auth/application/usecases/auth-usecases";
+import { compCatUsecases } from "@/features/comp-cat/application/usecases/comp-cat-usecases";
 import EvaluationBaseForm from "@/features/evaluation/presentation/views/EvaluationBaseForm";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { isLeft } from "fp-ts/lib/Either";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -9,7 +12,17 @@ async function AddEvaluationBaseServerLayer() {
   if (isLeft(authUser)) {
     redirect("/login");
   }
-  return <EvaluationBaseForm userId={authUser.right.userId} />;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: QUERY_KEYS.COMPETENCES.GET_ALL(),
+    queryFn: () =>
+      compCatUsecases.getCompetences({ userId: authUser.right.userId }),
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <EvaluationBaseForm userId={authUser.right.userId} />
+    </HydrationBoundary>
+  );
 }
 
 export default AddEvaluationBaseServerLayer;
