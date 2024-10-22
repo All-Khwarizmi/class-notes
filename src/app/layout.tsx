6c6a3859f89f63e98dca2f7e840a672a4e7e4565
Jason Suarez <swarecito@gmail.com>
2024-10-22 22:44:02 +0200

    💄 Update seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,9 @@ import { Toaster } from '@/core/components/ui/sonner';
 import QueryProvider from '@/core/query/QueryProvider';
 import { cn } from '@/lib/utils';
 import { Analytics } from '@vercel/analytics/react';
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import NextTopLoader from 'nextjs-toploader';
-import React from 'react';
 
 import './globals.css';
 import { Providers } from './providers';
@@ -17,52 +16,71 @@ const fontSans = FontSans({
   variable: '--font-sans',
 });
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: 'cover',
+  themeColor: '#000000',
+};
+
 export const metadata: Metadata = {
   title: 'La Classe - Votre site de prof en quelques clics',
   description:
-    'Créez votre site web professionnel sans effort. Centralisez vos ressources, dynamisez votre enseignement et modernisez votre présence en ligne. La Classe, la solution simple pour les enseignants qui veulent se démarquer.',
+    'La Classe permet aux enseignants de créer leur site web professionnel sans effort. Centralisez vos ressources, dynamisez votre enseignement et modernisez votre présence en ligne avec notre solution simple et intuitive.',
   keywords:
-    'site web enseignant, ressources pédagogiques, enseignement en ligne, modernisation éducative',
+    'La Classe, site web enseignant, ressources pédagogiques, enseignement en ligne, modernisation éducative, création site prof, plateforme enseignement, outils pédagogiques, présence numérique enseignant',
+  metadataBase: new URL('https://laclasse.app'),
   openGraph: {
-    type: 'website',
-    locale: 'fr_FR',
+    title: "La Classe - Créez votre site d'enseignant facilement",
+    description:
+      'La Classe permet aux enseignants de créer leur site web professionnel sans effort. La solution simple et intuitive pour les enseignants qui veulent se démarquer.',
     url: 'https://laclasse.app',
     siteName: 'La Classe',
-    title: 'La Classe - Votre site de prof en quelques clics',
-    description:
-      'Créez votre site web professionnel sans effort. La solution simple pour les enseignants qui veulent se démarquer.',
     images: [
       {
-        url: 'https://laclasse.app/og-image.jpg', // Assurez-vous de créer cette image
+        url: '/images/og-image.png',
         width: 1200,
         height: 630,
-        alt: 'La Classe - Votre site de prof',
+        alt: 'La Classe - Plateforme pour enseignants',
       },
     ],
+    locale: 'fr_FR',
+    type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'La Classe - Votre site de prof en quelques clics',
+    title: 'La Classe - Solution web pour enseignants',
     description:
-      'Créez votre site web professionnel sans effort. La solution simple pour les enseignants qui veulent se démarquer.',
-    images: ['https://laclasse.app/og-image.jpg'], // Utilisez la même image que pour OpenGraph
+      "Créez votre site web professionnel d'enseignant facilement. Centralisez vos ressources et modernisez votre enseignement.",
+    images: ['/images/og-image.png'],
+    creator: '@LaClasse',
+    site: '@LaClasseApp',
+  },
+  alternates: {
+    canonical: 'https://laclasse.app',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <Providers>
       <html lang="fr" suppressHydrationWarning>
-        <head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
-          />
-        </head>
         <body
           className={cn(
             'min-h-screen font-sans antialiased',
@@ -76,9 +94,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <NextTopLoader />
-            {/* <main className="flex fixed h-screen w-screen flex-col border-collapse overflow-scroll"></main> */}
             <Main>
-              {' '}
               <QueryProvider>{children}</QueryProvider>
             </Main>
             <Toaster richColors expand />
